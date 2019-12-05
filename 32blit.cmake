ba38b15228c6652c96bde4536763a03ffc357d67
Alistair Buxton <a.j.buxton@gmail.com>
2019-12-05 11:36:25 +0000

    Build 32blit elf/bin/hex with CMake
    cmake -DCMAKE_TOOLCHAIN_FILE=../32blit.toolchain ..

--- 32blit.cmake
+++ 32blit.cmake
@@ -1,8 +1,11 @@
 if (NOT DEFINED BLIT_ONCE)
 	set(BLIT_ONCE TRUE)
+
+	set(CMAKE_CXX_STANDARD 14)
+
 	add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/32blit 32blit)
 
-	if (${CMAKE_SYSTEM_NAME} STREQUAL 32blit)
+	if (${CMAKE_SYSTEM_NAME} STREQUAL Generic)
 		add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/32blit-stm32 32blit-stm32)
 	else()
 		add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/32blit-sdl 32blit-sdl)
