60844be05ab200d517a0382b616e425e8bac820d
Jonathan Williamson <jon@pimoroni.com>
2020-01-08 11:17:59 +0000

    Multi-channel, multi-voice audio kinda working. Example SID export player makes a racket...

--- examples/audio-test/build.stm32/CMakeFiles/tilt.elf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "tilt.elf.pdb"
-  "tilt.elf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tilt.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- examples/audio-test/build.stm32/CMakeFiles/tilt.elf.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tilt.elf.
-# This may be replaced when dependencies are built.
