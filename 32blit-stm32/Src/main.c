f5f1392a5bc0a397ee2a9a8919f23f7ded4381b9
Phil Howard <phil@gadgetoid.com>
2020-01-11 15:38:00 +0000

    Audio is currently incompatible with USB

--- 32blit-stm32/Src/main.c
+++ 32blit-stm32/Src/main.c
@@ -124,7 +124,7 @@ int main(void)
   MX_FATFS_Init();
   //MX_DMA2D_Init();
   MX_RNG_Init();
-  MX_USB_DEVICE_Init();
+  //MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
   blit_clear_framebuffer();
   blit_init();
