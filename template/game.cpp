7b49715d197cd6a9bb0ec3879d0f1d119880f153
Charlie Birks <charlie@daftgames.net>
2020-02-07 15:51:12 +0000

    Update template to fix build

--- template/game.cpp
+++ template/game.cpp
@@ -9,7 +9,7 @@ using namespace blit;
 // setup your game here
 //
 void init() {
-    set_screen_mode(screen_mode::hires);
+    set_screen_mode(ScreenMode::hires);
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -21,16 +21,16 @@ void init() {
 //
 void render(uint32_t time) {
 
-    // clear the screen -- fb is a reference to the frame buffer and can be used to draw all things with the 32blit
-    fb.clear();
+    // clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
+    screen.clear();
 
     // draw some text at the top of the screen
-    fb.alpha = 255;
-    fb.mask = nullptr;
-    fb.pen(rgba(255, 255, 255));
-    fb.rectangle(rect(0, 0, 320, 14));
-    fb.pen(rgba(0, 0, 0));
-    fb.text("Hello 32blit!", &minimal_font[0][0], point(5, 4));
+    screen.alpha = 255;
+    screen.mask = nullptr;
+    screen.pen(RGBA(255, 255, 255));
+    screen.rectangle(Rect(0, 0, 320, 14));
+    screen.pen(RGBA(0, 0, 0));
+    screen.text("Hello 32blit!", &minimal_font[0][0], Point(5, 4));
 }
 
 ///////////////////////////////////////////////////////////////////////////
