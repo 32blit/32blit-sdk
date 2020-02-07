74730e94c6e3d48a6e9a7f9ee0f28dd8b167b99d
Jonathan Williamson <jon@pimoroni.com>
2020-02-07 17:24:51 +0000

    Re-add some sprite blitting options

--- 32blit/graphics/blend.hpp
+++ 32blit/graphics/blend.hpp
@@ -13,16 +13,16 @@ namespace blit {
   // blends the pixel data in the source surface onto a span of pixels in the 
   // destination surface
   // supports source alpha, global alpha, and mask alpha where needed
-  typedef void(*BlitBlendFunc)(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt);
+  typedef void(*BlitBlendFunc)(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt, int32_t src_step);
 
   extern void RGBA_RGBA(const Pen* pen, const Surface* dest, uint32_t off, uint32_t cnt);
   extern void RGBA_RGB(const Pen* pen, const Surface* dest, uint32_t off, uint32_t cnt);
   extern void P_P(const Pen* pen, const Surface* dest, uint32_t off, uint32_t cnt);
   extern void M_M(const Pen* pen, const Surface* dest, uint32_t off, uint32_t cnt);
 
-  extern void RGBA_RGBA(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt);
-  extern void RGBA_RGB(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt);
-  extern void P_P(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt);
-  extern void M_M(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt);
+  extern void RGBA_RGBA(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt, int32_t src_step);
+  extern void RGBA_RGB(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt, int32_t src_step);
+  extern void P_P(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt, int32_t src_step);
+  extern void M_M(const Surface* src, uint32_t soff, const Surface* dest, uint32_t doff, uint32_t cnt, int32_t src_step);
 
 }
