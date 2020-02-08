0e13e2080cab61ca893e4f83ad9524fd532de2c0
Charlie Birks <charlie@daftgames.net>
2020-02-08 18:43:33 +0000

    Fix the sdl2-config file for non VS generators
    This should work as most of the other gemerators aren't multi-target

--- vs/sdl/sdl2-config.cmake
+++ vs/sdl/sdl2-config.cmake
@@ -1,2 +1,11 @@
 set(SDL2_INCLUDE_DIRS "${SDL2_DIR}/include")
-set(SDL2_LIBRARIES "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2.lib" "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2main.lib")
+
+if(${CMAKE_GENERATOR} MATCHES "Visual Studio")
+    set(SDL2_LIBRARIES "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2.lib" "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2main.lib")
+else()
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(SDL2_LIBRARIES "${SDL2_DIR}/lib/x64/SDL2.lib" "${SDL2_DIR}/lib/x64/SDL2main.lib")
+    else()
+        set(SDL2_LIBRARIES "${SDL2_DIR}/lib/x86/SDL2.lib" "${SDL2_DIR}/lib/x86/SDL2main.lib")
+    endif()
+endif()
