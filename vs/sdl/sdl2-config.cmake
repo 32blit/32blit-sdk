1ba062191792ba4458f9f2c509211837a0b5ad9b
Charlie Birks <charlie@daftgames.net>
2020-02-08 20:38:22 +0000

    Define path to the SDL dll in the config file

--- vs/sdl/sdl2-config.cmake
+++ vs/sdl/sdl2-config.cmake
@@ -1,11 +1,15 @@
 set(SDL2_INCLUDE_DIRS "${SDL2_DIR}/include")
 
 if(${CMAKE_GENERATOR} MATCHES "Visual Studio")
-    set(SDL2_LIBRARIES "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2.lib" "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2main.lib")
+    set(SDL2_LIBDIR "${SDL2_DIR}/lib/$(PlatformTarget)")
 else()
     if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-        set(SDL2_LIBRARIES "${SDL2_DIR}/lib/x64/SDL2.lib" "${SDL2_DIR}/lib/x64/SDL2main.lib")
+        set(SDL2_LIBDIR "${SDL2_DIR}/lib/x64/")
     else()
-        set(SDL2_LIBRARIES "${SDL2_DIR}/lib/x86/SDL2.lib" "${SDL2_DIR}/lib/x86/SDL2main.lib")
+        set(SDL2_LIBDIR "${SDL2_DIR}/lib/x86/")
     endif()
 endif()
+
+set(SDL2_LIBRARIES "${SDL2_LIBDIR}/SDL2.lib" "${SDL2_LIBDIR}/SDL2main.lib")
+
+set(SDL2_DLL "${SDL2_LIBDIR}/SDL2.dll")
