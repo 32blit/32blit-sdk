3b0edb216b493c6d65f15221d22f725877d5880d
Charlie Birks <charlie@daftgames.net>
2020-02-08 17:24:39 +0000

    Fix the vs sdl2-config file to not require moving x86 -> win32

--- vs/sdl/sdl2-config.cmake
+++ vs/sdl/sdl2-config.cmake
@@ -1,2 +1,2 @@
 set(SDL2_INCLUDE_DIRS "${SDL2_DIR}/include")
-set(SDL2_LIBRARIES "${SDL2_DIR}/lib/$(Platform)/SDL2.lib" "${SDL2_DIR}/lib/$(Platform)/SDL2main.lib")
+set(SDL2_LIBRARIES "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2.lib" "${SDL2_DIR}/lib/$(PlatformTarget)/SDL2main.lib")
