caa7ffae13a066de1609bc5ed4ed0111dcd0e8a3
Phil Howard <phil@gadgetoid.com>
2020-02-12 00:13:23 +0000

    Rebuild stale assets, move to 32blit.cmake

--- 32blit.cmake
+++ 32blit.cmake
@@ -10,6 +10,34 @@ if (NOT DEFINED BLIT_ONCE)
 
 	add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/32blit 32blit)
 
+	function(blit_asset NAME ASSET)
+		add_custom_command(
+			OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ASSET}.o
+			COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} && ${CMAKE_LINKER} -r -b binary -o ${CMAKE_CURRENT_BINARY_DIR}/${ASSET}.o ${ASSET}
+			COMMAND ${CMAKE_OBJCOPY} --rename-section .data=.rodata,alloc,load,readonly,data,contents ${CMAKE_CURRENT_BINARY_DIR}/${ASSET}.o ${CMAKE_CURRENT_BINARY_DIR}/${ASSET}.o
+			DEPENDS ${ASSET}
+		)
+		# Add object as a library
+		add_library(asset_${ASSET}
+			STATIC
+			${ASSET}.o)
+		# Tell CMake this object file has already been built by custom command
+		SET_SOURCE_FILES_PROPERTIES(
+			${ASSET}.o
+			PROPERTIES
+			EXTERNAL_OBJECT true
+			GENERATED true
+		)
+		# Avoid language warning
+		SET_TARGET_PROPERTIES(
+			asset_${ASSET}
+			PROPERTIES
+			LINKER_LANGUAGE C
+		)
+		# Link our asset to our output
+		target_link_libraries(${NAME} asset_${ASSET})
+	endfunction()
+
 	if (${CMAKE_SYSTEM_NAME} STREQUAL Generic)
 		set(FLASH_PORT "AUTO" CACHE STRING "Port to use for flash")
 
