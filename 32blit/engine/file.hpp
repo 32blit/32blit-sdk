66a60b05ed671619824c365c44506e4353ef9648
Charlie Birks <charlie@daftgames.net>
2020-02-19 11:07:24 +0000

    Add a File class
    Wraps the existing API

--- 32blit/engine/file.hpp
+++ 32blit/engine/file.hpp
@@ -21,4 +21,57 @@ namespace blit {
   extern uint32_t (*get_file_length)         (uint32_t fh);
 
   extern std::vector<FileInfo> (*list_files) (std::string path);
+  
+  class File final {
+  public:
+    File() {}
+    File(std::string filename) {open(filename);}
+    File(const File &) = delete;
+    File(File &&other) {
+      *this = std::move(other);
+    }
+
+    ~File() {
+      close();
+    }
+
+    File &operator=(const File &) = delete;
+
+    File &operator=(File &&other) {
+      if (this != &other) {
+        close();
+        std::swap(fh, other.fh);
+      }
+      return *this;
+    }
+
+    bool open(std::string file) {
+      close();
+      fh = open_file(file);
+      return fh != -1;
+    }
+
+    int32_t read(uint32_t offset, uint32_t length, char *buffer) {
+      return read_file(fh, offset, length, buffer);
+    }
+
+    void close() {
+      if(fh == -1)
+        return;
+
+      close_file(fh);
+      fh = -1;
+    }
+
+    uint32_t get_length() {
+      return get_file_length(fh);
+    }
+
+    bool is_open() const {
+      return fh != -1;
+    }
+
+  private:
+    int32_t fh = -1;
+  };
 }
