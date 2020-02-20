72af85dc27688cd48146f80636d1a94d8cf0944b
Charlie Birks <charlie@daftgames.net>
2020-02-20 11:48:39 +0000

    Hide the old API

--- 32blit-sdl/File.hpp
+++ 32blit-sdl/File.hpp
@@ -12,3 +12,10 @@ int32_t read_file(uint32_t fh, uint32_t offset, uint32_t length, char *buffer);
 int32_t close_file(uint32_t fh);
 uint32_t get_file_length(uint32_t fh);
 std::vector<blit::FileInfo> list_files(std::string path);
+
+namespace blit {
+  extern int32_t  (*open_file)               (std::string file);
+  extern int32_t  (*read_file)               (uint32_t fh, uint32_t offset, uint32_t length, char* buffer);
+  extern int32_t  (*close_file)              (uint32_t fh);
+  extern uint32_t (*get_file_length)         (uint32_t fh);
+}
