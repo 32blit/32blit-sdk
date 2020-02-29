c00f2e82d1372db47e6d01934982c4c3bb15eda9
Charlie Birks <charlie@daftgames.net>
2020-02-29 18:07:37 +0000

    Add a constructor taking a single array to Font

--- 32blit/graphics/font.hpp
+++ 32blit/graphics/font.hpp
@@ -6,6 +6,8 @@ namespace blit {
   struct Font {
     constexpr Font(const uint8_t *data, const uint8_t *char_w_variable, uint8_t char_w, uint8_t char_h, uint8_t spacing_y = 1)
       : data(data), char_w(char_w), char_h(char_h), char_w_variable(char_w_variable), spacing_y(spacing_y) {}
+    
+    constexpr Font(const uint8_t *data) : data(data + 8 + data[4]), char_w(data[5]), char_h(data[6]), char_w_variable(data + 8), spacing_y(data[7]) {}
 
     const uint8_t *data;
 
