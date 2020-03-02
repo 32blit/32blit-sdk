735d0968c69563a436daaefbd3fd81945a5787cb
Charlie Birks <charlie@daftgames.net>
2020-03-02 17:38:17 +0000

    Handle including spi-st7272a from C++

--- 32blit-stm32/Inc/spi-st7272a.h
+++ 32blit-stm32/Inc/spi-st7272a.h
@@ -8,6 +8,14 @@
 #define ST7272A_CS(active)            if(active) {LOW(LCD_CS_GPIO_Port, LCD_CS_Pin)} else {HIGH(LCD_CS_GPIO_Port, LCD_CS_Pin)}
 #define ST7272A_RESET()               LOW(LCD_RESET_GPIO_Port, LCD_RESET_Pin); DELAY(100); HIGH(LCD_RESET_GPIO_Port, LCD_RESET_Pin); DELAY(100);
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern void st7272a_set_bgr();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
