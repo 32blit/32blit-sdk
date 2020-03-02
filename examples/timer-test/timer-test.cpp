216eb7008c4aaf13a118379f28f4e321753d9efa
Phil Howard <phil@gadgetoid.com>
2020-03-02 21:55:15 +0000

    Code cleanup, double promo fixes

--- examples/timer-test/timer-test.cpp
+++ examples/timer-test/timer-test.cpp
@@ -57,8 +57,7 @@ void render(uint32_t time_ms) {
   // Since our timer callback is updating our `count` variable
   // we can just display it on the screen and watch it tick up!
   screen.pen = Pen(255, 255, 255);
-  sprintf(text_buffer, "Count: %d", count);
-  screen.text(text_buffer, minimal_font, Point(120, 100));
+  screen.text("Count: " + std::to_string(count), minimal_font, Point(120, 100));
 
   // `is_running()` is a handy shorthand for checking the timer state
   if(timer_count.is_running()) {
