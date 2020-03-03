d9e7094c83f0f1d839748392ffaa8ccabfcf7b2a
Phil Howard <phil@gadgetoid.com>
2020-03-03 22:18:15 +0000

    400KHz i2c with slight rise/fall slew

--- 32blit-stm32/Src/i2c.c
+++ 32blit-stm32/Src/i2c.c
@@ -31,7 +31,7 @@ void MX_I2C4_Init(void)
 {
 
   hi2c4.Instance = I2C4;
-  hi2c4.Init.Timing = 0x307075B1;
+  hi2c4.Init.Timing = 0x20401943;
   hi2c4.Init.OwnAddress1 = 0;
   hi2c4.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c4.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
