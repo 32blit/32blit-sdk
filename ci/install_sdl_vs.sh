c62e40aad45707f2180b18df5c6565c352dbbd03
Charlie Birks <charlie@daftgames.net>
2020-06-07 13:44:49 +0100

    Install SDL_net for CI

--- ci/install_sdl_vs.sh
+++ ci/install_sdl_vs.sh
@@ -1,11 +1,14 @@
-if [ ! -f vs/sdl/include/SDL_image.h ]; then
+if [ ! -f vs/sdl/include/SDL_net.h ]; then
     curl https://libsdl.org/release/SDL2-devel-2.0.10-VC.zip -o SDL2.zip
     curl https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip -o SDL2_image.zip
+    curl https://www.libsdl.org/projects/SDL_net/release/SDL2_net-devel-2.0.1-VC.zip -o SDL2_net.zip
 
     unzip SDL2.zip -d vs/sdl
-    unzip SDL2_image.zip -d vs/sdl
+    unzip -o SDL2_image.zip -d vs/sdl
+    unzip -o SDL2_net.zip -d vs/sdl
 
     # move dirs up
     mv vs/sdl/SDL2-2.0.10/* vs/sdl
     cp -r vs/sdl/SDL2_image-2.0.5/* vs/sdl
+    cp -r vs/sdl/SDL2_net-2.0.1/* vs/sdl
 fi
