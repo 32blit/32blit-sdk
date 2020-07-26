f12cceab59b73530451a4e87b8274d83a4fb6dd9
Charlie Birks <charlie@daftgames.net>
2020-07-26 22:24:02 +0100

    Set _FS_NORTC
    We have no RTC, so files were being written with invalid timestamps.

--- 32blit-stm32/Inc/ffconf.h
+++ 32blit-stm32/Inc/ffconf.h
@@ -214,7 +214,7 @@
 /  When enable exFAT, also LFN needs to be enabled. (_USE_LFN >= 1)
 /  Note that enabling exFAT discards C89 compatibility. */
 
-#define _FS_NORTC	0
+#define _FS_NORTC	1
 #define _NORTC_MON	6
 #define _NORTC_MDAY	4
 #define _NORTC_YEAR	2015
