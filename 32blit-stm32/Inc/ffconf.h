b3de30db99b7f5c74c879d4b887f179a990a30b0
Charlie Birks <charlie@daftgames.net>
2020-07-26 22:50:10 +0100

    Disable _USE_FASTSEEK
    It's just dead code without using CREATE_LINKMAP anywhere...

--- 32blit-stm32/Inc/ffconf.h
+++ 32blit-stm32/Inc/ffconf.h
@@ -60,7 +60,7 @@
 #define _USE_MKFS            0
 /* This option switches f_mkfs() function. (0:Disable or 1:Enable) */
 
-#define _USE_FASTSEEK        1
+#define _USE_FASTSEEK        0
 /* This option switches fast seek feature. (0:Disable or 1:Enable) */
 
 #define	_USE_EXPAND		0
