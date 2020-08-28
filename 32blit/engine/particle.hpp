efe965856c98768b2c442b4ade09da59768dc509
Charlie Birks <charlie@daftgames.net>
2020-08-28 11:49:03 +0100

    Move particle classes to the namespace

--- 32blit/engine/particle.hpp
+++ 32blit/engine/particle.hpp
@@ -5,30 +5,31 @@
 #include <cstdint>
 #include "../types/vec2.hpp"
 
-struct Particle {
-  blit::Vec2 pos;
-  blit::Vec2 vel;
-  float age;
+namespace blit {
+  struct Particle {
+    blit::Vec2 pos;
+    blit::Vec2 vel;
+    float age;
 
-  uint32_t age_ms = 0;
+    uint32_t age_ms = 0;
 
-  Particle(blit::Vec2 pos, blit::Vec2 vel) : pos(pos), vel(vel) {};
-};
+    Particle(blit::Vec2 pos, blit::Vec2 vel) : pos(pos), vel(vel) {};
+  };
 
-struct ParticleGenerator {
-  uint32_t count;
-  uint32_t lifetime_ms;
-  uint32_t generate_per_ms;
-  uint32_t remaining_ms;
-  blit::Vec2 force;
+  struct ParticleGenerator {
+    uint32_t count;
+    uint32_t lifetime_ms;
+    uint32_t generate_per_ms;
+    uint32_t remaining_ms;
+    blit::Vec2 force;
 
-  std::function<Particle*(void)> generate;
+    std::function<Particle*(void)> generate;
 
-  std::deque<Particle *> particles;
+    std::deque<Particle *> particles;
 
-  ParticleGenerator(uint32_t count, uint32_t lifetime_ms, std::function<Particle *(void)> generate);
-  ~ParticleGenerator();
+    ParticleGenerator(uint32_t count, uint32_t lifetime_ms, std::function<Particle *(void)> generate);
+    ~ParticleGenerator();
 
-  void update(uint32_t time_ms);
-};
-
+    void update(uint32_t time_ms);
+  };
+}
