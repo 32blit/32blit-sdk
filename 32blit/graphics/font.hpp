ba7e2ac88eba9f9887ab9b452bfe5a18d2ff0787
Charlie Birks <charlie@daftgames.net>
2020-09-10 16:15:58 +0100

    More Font docs

--- 32blit/graphics/font.hpp
+++ 32blit/graphics/font.hpp
@@ -7,15 +7,19 @@ namespace blit {
     constexpr Font(const uint8_t *data, const uint8_t *char_w_variable, uint8_t char_w, uint8_t char_h, uint8_t spacing_y = 1)
       : data(data), char_w(char_w), char_h(char_h), spacing_y(spacing_y), char_w_variable(char_w_variable) {}
     
+    /// Create a font from a packed font asset
     constexpr Font(const uint8_t *data) : data(data + 8 + data[4]), char_w(data[5]), char_h(data[6]), spacing_y(data[7]), char_w_variable(data + 8) {}
 
+    /// Character data (packed 1 bit per pixel)
     const uint8_t *data;
 
-    // fixed size
+    /// Fixed character size
     uint8_t char_w, char_h;
+
+    /// Extra line spacing
     uint8_t spacing_y;
 
-    // variable width
+    /// Individual character widths
     const uint8_t *char_w_variable;
   };
 
