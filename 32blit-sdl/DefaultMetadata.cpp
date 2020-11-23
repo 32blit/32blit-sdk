bbd74d55da6a77bd0c1c9edad1a61bb35ae81173
Charlie Birks <charlie@daftgames.net>
2020-11-23 19:24:59 +0000

    Include metadata strings in SDL builds
    Generates a file containing the definitions. The library contains some
    defaults that are marked weak for GCC and relies on the VS linker
    behavior of OBJs overriding LIBs.

---
+++ 32blit-sdl/DefaultMetadata.cpp
@@ -0,0 +1,12 @@
+// Default metadata if there is non compiled into the game
+// There should be nothing else in this file so that the VS linker drops it when not needed
+#ifdef _MSC_VER
+#define WEAK
+#else
+#define WEAK [[gnu::weak]]
+#endif
+
+WEAK const char *metadata_title = "32Blit Game";
+WEAK const char *metadata_author = "Unknown";
+WEAK const char *metadata_description = "";
+WEAK const char *metadata_version = "v0.0.0";
