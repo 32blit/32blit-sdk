32blit/32blit-sdk

commit 71195ae3e46923f6a4c40caaf74ca98aba1ceb11

Move auto-launch on reset to the firmware

32blit-stm32/Src/main.c
@@ -156,8 +156,9 @@ int main(void)
   blit_init();
 
 #if (INITIALISE_QSPI==1)
-  if((persist.reset_target == prtGame) && HAL_GPIO_ReadPin(BUTTON_MENU_GPIO_Port,  BUTTON_MENU_Pin) && !persist.reset_error)
-    blit_switch_execution(persist.last_game_offset);
+  // don't switch to game if it crashed, or menu is held
+  if(persist.reset_target == prtGame && (!HAL_GPIO_ReadPin(BUTTON_MENU_GPIO_Port,  BUTTON_MENU_Pin) || persist.reset_error))
+    persist.reset_target = prtFirmware;
 #endif
 
   // add CDC handler to reset device on receiving "_RST" and "SWIT"

firmware/firmware.cpp
@@ -297,14 +297,16 @@ void init() {
 
   g_commandStream.AddCommandHandler(CDCCommandHandler::CDCFourCCMake<'E', 'R', 'S', 'E'>::value, &cdc_erase_handler);
 
+  // auto-launch
+  if(persist.reset_target == prtGame)
+    blit_switch_execution(persist.last_game_offset);
+
   // error reset handling
   if(persist.reset_error) {
     dialog.show("Oops!", "Restart game?", [](bool yes){
 
       if(yes)
         blit_switch_execution(persist.last_game_offset);
-      else
-        persist.reset_target = prtFirmware;
 
       persist.reset_error = false;
     });

commit 31d029b50b35529a8f793f767a16e775dbe50d72

Fix HOME+reset and crash reset
Broken in 71195ae3e46923f6a4c40caaf74ca98aba1ceb11 as that check happens
after the firmware has done the switch.

(Though somehow stull seemed to work sometimes...)

32blit-stm32/Src/32blit.cpp
@@ -366,6 +366,12 @@ void blit_init() {
       persist.last_game_offset = 0;
     }
 
+#if (INITIALISE_QSPI==1)
+    // don't switch to game if it crashed, or menu is held
+    if(persist.reset_target == prtGame && (!HAL_GPIO_ReadPin(BUTTON_MENU_GPIO_Port,  BUTTON_MENU_Pin) || persist.reset_error))
+      persist.reset_target = prtFirmware;
+#endif
+
     init_api_shared();
 
     blit_update_volume();

32blit-stm32/Src/main.c
@@ -155,12 +155,6 @@ int main(void)
 
   blit_init();
 
-#if (INITIALISE_QSPI==1)
-  // don't switch to game if it crashed, or menu is held
-  if(persist.reset_target == prtGame && (!HAL_GPIO_ReadPin(BUTTON_MENU_GPIO_Port,  BUTTON_MENU_Pin) || persist.reset_error))
-    persist.reset_target = prtFirmware;
-#endif
-
   // add CDC handler to reset device on receiving "_RST" and "SWIT"
 	g_commandStream.AddCommandHandler(CDCCommandHandler::CDCFourCCMake<'_', 'R', 'S', 'T'>::value, &g_resetHandler);
 	g_commandStream.AddCommandHandler(CDCCommandHandler::CDCFourCCMake<'S', 'W', 'I', 'T'>::value, &g_resetHandler);