313a1e1da8bb00a62cf1f6c90d50bc7d8f508f03
shane-powell <kenjara@hotmail.com>
2020-12-09 08:10:01 +0000

    one liner build script.
    Updates 32Blit.
    Removes and recreates build folder and builds everything.
    Moves all .blits into single blits folder within build directory.

---
+++ build.sh
@@ -0,0 +1,8 @@
+pip3 install --upgrade 32blit
+rm -r build.stm32/
+mkdir build.stm32
+cd build.stm32
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../32blit.toolchain
+make
+mkdir  blits
+cp examples/*/*.blit blits/
