90f14af4ecf3d62c749779a15715ab67ede03714
shane-powell <kenjara@hotmail.com>
2020-12-09 10:38:54 +0000

    Moved build.sh to tools. Updated paths so it would run from new location.

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,7 @@
 pip3 install --upgrade 32blit
-rm -r build.stm32/
-mkdir build.stm32
-cd build.stm32
+rm -r ../build.stm32/
+mkdir ../build.stm32
+cd ../build.stm32
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../32blit.toolchain
 make
 mkdir  blits
