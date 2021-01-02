08461340d21e847abf1229227a6b6b5f3546867b
Phil Howard <phil@gadgetoid.com>
2021-01-02 22:08:15 +0000

    Fix text wrapping

--- launcher/launcher.cpp
+++ launcher/launcher.cpp
@@ -376,12 +376,12 @@ void render(uint32_t time) {
       screen.blit(selected_game_metadata.splash, Rect(Point(0, 0), selected_game_metadata.splash->bounds), game_info_offset);
 
     screen.pen = theme.color_accent;
-    std::string wrapped_title = screen.wrap_text(selected_game_metadata.title, screen.bounds.w - game_info_offset.x - 10, minimal_font, false, true);
+    std::string wrapped_title = screen.wrap_text(selected_game_metadata.title, screen.bounds.w - game_info_offset.x - 10, minimal_font);
 
-    Size title_size = screen.measure_text(wrapped_title, minimal_font, false);
+    Size title_size = screen.measure_text(wrapped_title, minimal_font);
     screen.text(wrapped_title, minimal_font, Point(game_info_offset.x, game_info_offset.y + 104));
 
-    Rect desc_rect(game_info_offset.x, game_info_offset.y + 104 + title_size.h, screen.bounds.w - game_info_offset.x - 10, 64);
+    Rect desc_rect(game_info_offset.x, game_info_offset.y + 108 + title_size.h, screen.bounds.w - game_info_offset.x - 10, 64);
 
     screen.pen = theme.color_text;
     std::string wrapped_desc = screen.wrap_text(selected_game_metadata.description, desc_rect.w, minimal_font);
