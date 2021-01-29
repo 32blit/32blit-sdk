38c8d4ff8fcbfea580ea90c254ba03e5d0e77859
Charlie Birks <charlie@daft.games>
2021-01-29 18:09:21 +0000

    Remove unused code from CDCDataStream

--- 32blit-stm32/Inc/CDCDataStream.h
+++ 32blit-stm32/Inc/CDCDataStream.h
@@ -13,39 +13,6 @@
 #include <streambuf>
 #include <istream>
 
-class CDCMemBuffer : public std::basic_streambuf<char>
-{
-public:
-	CDCMemBuffer(const uint8_t *pData, size_t uLen)
-	{
-    setg((char*)pData, (char*)pData, (char*)pData + uLen);
-  }
-
-	void SetData(const uint8_t *pData, size_t uLen)
-	{
-    setg((char*)pData, (char*)pData, (char*)pData + uLen);
-  }
-};
-
-class CDCMemoryStream : public std::istream
-{
-public:
-	CDCMemoryStream(const uint8_t *pData = NULL , size_t uLen = 0) : std::istream(&m_buffer), m_buffer(pData, uLen)
-	{
-    rdbuf(&m_buffer);
-  }
-
-	void SetData(const uint8_t *pData, size_t uLen)
-	{
-		clear();
-		m_buffer.SetData(pData, uLen);
-    rdbuf(&m_buffer);
-	}
-private:
-	CDCMemBuffer m_buffer;
-};
-
-
 class CDCDataStream
 {
 public:
@@ -66,41 +33,6 @@ public:
 	uint32_t GetStreamLength(void)
 	{
 		return m_uLen;
-	}
-
-
-	bool GetMemoryStream(CDCMemoryStream &ms)
-	{
-		bool bResult = false;
-
-		uint8_t *pData;
-		uint32_t uLen;
-		if(uLen)
-		{
-			ms.SetData(pData, uLen);
-			bResult = true;
-		}
-
-		return bResult;
-	}
-
-
-	bool GetStreamData(uint8_t **ppData, uint32_t *puLen)
-	{
-		bool bResult = true;
-
-		if(m_uLen)
-		{
-			*ppData = m_pData;
-			*puLen = m_uLen;
-			m_uLen = 0;
-		}
-		else
-		{
-			bResult = false;
-		}
-
-		return bResult;
 	}
 
 	bool GetDataOfLength(void *pData, uint8_t uLen)
