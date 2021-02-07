a79be8dbe061cbf4f8e7d35919a6054e3fc6110e
Alistair Buxton <a.j.buxton@gmail.com>
2021-02-07 03:02:29 +0000

    Use find_package instead of import in the backwards-compat
    This guarantees that 32BLIT_DIR will always be set, even if user
    didn't set it.

--- 32blit.cmake
+++ 32blit.cmake
@@ -1,8 +1,8 @@
-message(DEPRECATION "\
+message (DEPRECATION "\
 Please update your 32Blit cmake project to use:
     find_package(32BLIT CONFIG REQUIRED /path/to/32blit-beta)
 instead of:
     include(32blit.cmake)
 and use the variable 32BLIT_DIR instead of 32BLIT_PATH.
 ")
-include(${CMAKE_CURRENT_LIST_DIR}/32blit-config.cmake)
+find_package (32BLIT CONFIG REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR})
