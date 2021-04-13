bdb032c756521da1f15aff66e83e64d73a99a6ba
Charlie Birks <charlie@daft.games>
2021-04-13 22:40:44 +0100

    Remove some redundant externs
    Also inconsistent

--- 32blit-stm32/Inc/32blit.h
+++ 32blit-stm32/Inc/32blit.h
@@ -11,39 +11,39 @@
 extern bool is_beta_unit;
 
 // Functions defined by user code files
-extern void init();
-extern void update(uint32_t time);
-extern void render(uint32_t time);
+void init();
+void update(uint32_t time);
+void render(uint32_t time);
 
 // SD storage
-extern char *get_fr_err_text(FRESULT err);
-extern bool blit_sd_detected();
-extern bool blit_sd_mounted();
+char *get_fr_err_text(FRESULT err);
+bool blit_sd_detected();
+bool blit_sd_mounted();
 
-extern void render_yield();
+void render_yield();
 
 // Blit setup and main loop
-extern void blit_tick();
-extern void blit_init();
+void blit_tick();
+void blit_init();
 
 // IO
 void blit_update_volume();
-extern void blit_update_vibration();
-extern void blit_update_led();
-extern void blit_process_input();
+void blit_update_vibration();
+void blit_update_led();
+void blit_process_input();
 
 // Switching execution.
 // Address is relative to the start of flash, ignored if switching to firmware
-extern bool blit_switch_execution(uint32_t address, bool force_game);
-extern bool blit_user_code_running();
+bool blit_switch_execution(uint32_t address, bool force_game);
+bool blit_user_code_running();
 extern "C" void blit_reset_with_error();
-extern void blit_enable_user_code();
-extern void blit_disable_user_code();
-extern RawMetadata *blit_get_running_game_metadata();
+void blit_enable_user_code();
+void blit_disable_user_code();
+RawMetadata *blit_get_running_game_metadata();
 
 void blit_menu_update(uint32_t time);
 void blit_menu_render(uint32_t time);
 void blit_menu();
 
-extern void blit_enable_ADC();
-extern void blit_disable_ADC();
+void blit_enable_ADC();
+void blit_disable_ADC();
