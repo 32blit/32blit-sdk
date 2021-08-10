2efa0a829116d81c37596dec3452f29391b32798
Phil Howard <phil@gadgetoid.com>
2021-08-10 23:59:43 +0100

    Geometry: accurate polygon area and centroid
    Drop the old rectangle bounds approach and calculate the polygon area and centroid "correctly."
    
    Some tweaks to velocity/rotation imparted upon asteroid halves when split.

--- examples/geometry/geometry.cpp
+++ examples/geometry/geometry.cpp
@@ -6,22 +6,22 @@
 using namespace blit;
 
 #define __AUDIO__
+//#define __DEBUG__
 
-#define COLLECT_THRESHOLD (unsigned short)150
-#define DISCARD_THRESHOLD (unsigned short)20
 
-const uint32_t STARTING_ENERGY = 500;
-const uint32_t LASER_COST = 100;
-const uint32_t MOVEMENT_COST = 5;
-const uint32_t DAMAGE_COST = 10;
+const int16_t STARTING_ENERGY = 500;
+const int16_t REGEN_ENERGY = 1;
+const int16_t LASER_COST = 50;
+const int16_t MOVEMENT_COST = 3;
+const int16_t DAMAGE_COST = 10;
 const uint8_t STARTING_LIVES = 3;
 const uint8_t P_MAX_AGE = 255;
 
-#define ASTEROID_COUNT (unsigned short)8
-#define ASTEROID_MIN_SIZE (unsigned short)4
-#define ASTEROID_MAX_SIZE (unsigned short)15
+const uint8_t ASTEROID_COUNT = 5;
+const uint8_t ASTEROID_MIN_R = 40;
+const uint8_t ASTEROID_MAX_R = 50;
 
-#define POLYGON_COUNT (unsigned int)10
+const uint16_t ASTEROID_MIN_AREA = 100;
 
 struct SpaceDust {
     blit::Vec2 pos;
@@ -46,6 +46,18 @@ struct player {
     Vec2 shot_target;
     uint8_t lives = 3;
     bool invincible = false;
+
+    void reset_or_die() {
+        energy = STARTING_ENERGY;
+        if(lives == 0) {
+            lives = STARTING_LIVES;
+            score = 0;
+        }
+        invincible = true;
+        position = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
+        rotation = 0.0f;
+        rotational_velocity = 0.0f;
+    }
 };
 
 struct polygon {
@@ -55,6 +67,7 @@ struct polygon {
     Vec2 origin;
     std::vector<Vec2> points;
     bool prune = false;
+    uint16_t area = 0;
 };
 
 std::vector<SpaceDust> particles;
@@ -62,10 +75,11 @@ std::vector<polygon> polygons;
 
 player player1;
 
-void explode(Vec2 origin) {
+void explode(Vec2 origin, float factor=1.0f) {
     channels[2].frequency = 800;
     channels[2].trigger_attack();
     uint8_t count = 5 + (blit::random() % 25);
+    count *= factor;
     for(auto x = 0u; x < count; x++) {
         float r = blit::random() % 360;
         float v = (50.0f + (blit::random() % 100)) / 100.0f;
@@ -80,31 +94,43 @@ void explode(Vec2 origin) {
     }
 }
 
+void thrust(Vec2 origin, Vec2 vel) {
+    vel.rotate(pi);
+    for(auto x = 0u; x < 10; x++) {
+        float r = ((blit::random() % 300) - 150)  / 5000.0f;
+        vel.rotate(r);
+        particles.push_back(SpaceDust(
+            origin,
+            vel,
+            255
+        ));
+    }
+}
+
 bool prune_polygons(polygon p){
     if(p.prune) {
-        explode(p.origin);
+        explode(p.origin, p.area / float(ASTEROID_MIN_AREA));
     }
     return p.prune;
 }
 
-Rect bounds_of_polygon(std::vector<Vec2> &points) {
-    float xmin=1000000, xmax=-1000000, ymin=1000000, ymax=-1000000;
-
-    for (auto &p : points) {
-        if (p.x < xmin) xmin=p.x;
-        if (p.x > xmax) xmax=p.x;
-        if (p.y < ymin) ymin=p.y;
-        if (p.y > ymax) ymax=p.y;
+uint16_t area_of_polygon(std::vector<Vec2> &points) {
+    auto a = points.back();
+    uint16_t area = 0;
+    for (auto b : points) {
+        area += ((a.x * b.y) - (b.x * a.y)) / 2u;
+        a = b;
     }
-    return Rect(xmin, ymin, xmax-xmin, ymax-ymin);
+    // Fix for infinitely small poygons shooting to 65535
+    return area > 60000 ? 0 : area;
 }
 
-Vec2 middle_of_polygon(std::vector<Vec2> &points) {
-    Rect bounds = bounds_of_polygon(points);
-    return Vec2(
-        bounds.x + (bounds.w / 2),
-        bounds.y + (bounds.h / 2)
-    );
+Vec2 centroid_of_polygon(std::vector<Vec2> &points) {
+    Vec2 t(0, 0);
+    for (auto p : points) {
+        t += p;
+    }
+    return t / points.size();
 }
 
 bool line_segment_intersection(Vec2 *intersection, Vec2 a, Vec2 b, Vec2 c, Vec2 d) {
@@ -126,6 +152,7 @@ bool line_segment_intersection(Vec2 *intersection, Vec2 a, Vec2 b, Vec2 c, Vec2 
 polygon split_polygon(polygon *poly, Vec2 a, Vec2 b) {
     std::vector<Vec2> split_a;
     std::vector<Vec2> split_b;
+    std::vector<Vec2> split_line_segment;
 
     Vec2 last_point = poly->points.back();
 
@@ -133,11 +160,14 @@ polygon split_polygon(polygon *poly, Vec2 a, Vec2 b) {
         float bside = ((p.x - a.x) * (b.y - a.y)) - ((p.y - a.y) * (b.x - a.x));
         float last_bside = ((last_point.x - a.x) * (b.y - a.y)) - ((last_point.y - a.y) * (b.x - a.x));
 
+        // If our polygon line croses out intersection line, create a new point along the intersection for each new polygon
         if (bside != last_bside) {
             Vec2 intersection;
             if (line_segment_intersection(&intersection, last_point, p, a, b)) {
-                split_a.push_back(Vec2(intersection.x, intersection.y));
-                split_b.push_back(Vec2(intersection.x, intersection.y));
+                split_a.emplace_back(intersection);
+                split_b.emplace_back(intersection);
+                split_line_segment.emplace_back(intersection);
+                explode(Vec2(intersection.x, intersection.y), 0.25f);
             }
         }
 
@@ -151,34 +181,55 @@ polygon split_polygon(polygon *poly, Vec2 a, Vec2 b) {
         last_point = p;
     }
 
-    if (split_b.size() > 0) {
-        poly->points = std::vector<Vec2>(split_a);
-        poly->origin = middle_of_polygon(poly->points);
-        poly->rotational_velocity += 0.005f;
+    // If we don't encounter two line segment intersections, we're shooting from *inside* an asteroid
+    if (split_b.size() > 0 && split_line_segment.size() == 2) {
 
-        Rect bounds = bounds_of_polygon(poly->points);
-        if((bounds.w * bounds.h) < 70) {
-            player1.score += bounds.w * bounds.h;
+        poly->points = std::vector<Vec2>(split_a);
+        poly->origin = centroid_of_polygon(poly->points);
+        //poly->rotational_velocity += 0.005f;
+        poly->area = area_of_polygon(poly->points);
+
+        if(poly->area < ASTEROID_MIN_AREA) {
+            player1.score += poly->area;
             poly->prune = true;
         } else {
             poly->prune = false;
         }
 
-        bounds = bounds_of_polygon(split_b);
-        if((bounds.w * bounds.h) >= 70) {
+        float area = area_of_polygon(split_b);
+        Vec2 midpoint = split_line_segment.front() + split_line_segment.back();
+        midpoint /= 2.0f;
+        Vec2 original_velocity = poly->velocity;
+
+        poly->velocity = poly->origin - midpoint;
+        poly->velocity.normalize();
+        poly->velocity *= 0.1f;
+        poly->velocity += original_velocity;
+        poly->rotational_velocity = poly->origin.angle(midpoint) * 0.1f;
+
+        if(area >= ASTEROID_MIN_AREA) {
+
             polygon new_polygon;
             new_polygon.colour_offset = poly->colour_offset;
-            new_polygon.velocity = poly->velocity * -1;
             new_polygon.points = std::vector<Vec2>(split_b);
-            new_polygon.origin = middle_of_polygon(new_polygon.points);
-            new_polygon.rotational_velocity = -poly->rotational_velocity;
+            new_polygon.origin = centroid_of_polygon(new_polygon.points);
+
+            // Push the new asteroid half away from the "cut"
+            new_polygon.velocity = new_polygon.origin - midpoint;
+            new_polygon.velocity.normalize();
+            new_polygon.velocity *= 0.1f;
+            new_polygon.velocity += original_velocity;
+
+            new_polygon.rotational_velocity = new_polygon.origin.angle(midpoint) * 0.1f;
+
+            new_polygon.area = area;
             return new_polygon;
         }
         else {
-            player1.score += bounds.w * bounds.h;
+            player1.score += area;
             // This polygon doesn't ever exist, but since we have the list
             // of points and its bounds we can emenate an explosion from its center
-            explode(Vec2(bounds.center()));
+            explode(centroid_of_polygon(split_b), area / float(ASTEROID_MIN_AREA));
         }
     }
     
@@ -187,12 +238,11 @@ polygon split_polygon(polygon *poly, Vec2 a, Vec2 b) {
 }
 
 void draw_polygon(std::vector<Vec2> &points) {
-    Vec2 last_point = points[0];
+    Vec2 last_point = points.back();
     for (auto &p : points) {
         screen.line(last_point, p);
         last_point = p;
     }
-    screen.line(last_point, points[0]);
 }
 
 std::vector<Vec2> random_convex_polygon(Vec2 origin, float radius) {
@@ -239,34 +289,27 @@ void translate_polygon(std::vector<Vec2> &points, Vec2 translation) {
     }
 }
 
-void rotate_polygon(std::vector<Vec2> &points, float angle) {
-    Vec2 origin = middle_of_polygon(points);
-    rotate_polygon(points, angle, origin);
-}
-
 void init() {
     set_screen_mode(ScreenMode::hires);
-    for(unsigned int i = 0; i < POLYGON_COUNT; i++){
+    for(unsigned int i = 0; i < ASTEROID_COUNT; i++){
         polygon p;
         float x = rand() % screen.bounds.w;
         float y = rand() % screen.bounds.h;
         //float x = screen.bounds.w / 2;
         //float y = screen.bounds.h / 2;
-        float r = (rand() % 20) + 10;
-        float vx = ((rand() % 10) - 5) / 25.0;
-        float vy = ((rand() % 10) - 5) / 25.0;
-        p.colour_offset = (rand() % 100) / 100.0;
+        float r = (rand() % (ASTEROID_MAX_R - ASTEROID_MIN_R)) + ASTEROID_MIN_R;
+        float vx = ((rand() % 10) - 5) / 25.0f;
+        float vy = ((rand() % 10) - 5) / 25.0f;
+        p.colour_offset = (rand() % 100) / 100.0f;
         p.points = random_convex_polygon(Vec2(x, y), r);
         p.velocity = Vec2(vx, vy);
-        p.origin = middle_of_polygon(p.points);
-        p.rotational_velocity = ((rand() % 20) - 10) / (float)360.0;
+        p.origin = centroid_of_polygon(p.points);
+        p.rotational_velocity = ((rand() % 10) - 5) / 1000.0f;
+        p.area = area_of_polygon(p.points);
         polygons.push_back(p);
     }
 
-    player1.energy = STARTING_ENERGY;
-    player1.lives = STARTING_LIVES;
-    player1.invincible = true;
-    player1.position = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
+    player1.reset_or_die();
     player1.shape.push_back(Vec2(0, -6));
     player1.shape.push_back(Vec2(-6, 6));
     player1.shape.push_back(Vec2(0, 2));
@@ -300,7 +343,9 @@ void init() {
 }
 
 void render(uint32_t time) {
+#ifdef __DEBUG__
     uint32_t ms_start = now();
+#endif
     float h = time / (pi * 2) / 100.0f;
 
     screen.pen = Pen(0, 0, 0);
@@ -314,15 +359,19 @@ void render(uint32_t time) {
         screen.pen = Pen(255, 255, 255);
     }
     std::vector<Vec2> player1_shape(player1.shape);
-    rotate_polygon(player1_shape, player1.rotation);
     translate_polygon(player1_shape, player1.position);
+    rotate_polygon(player1_shape, player1.rotation, player1.position);
     draw_polygon(player1_shape);
 
     for(auto &p: polygons){
         Pen c = hsv_to_rgba(h / (pi * 2) + p.colour_offset, 1.0, 1.0);
         screen.pen = c;
         draw_polygon(p.points);
-        //screen.pixel(p.origin);
+#ifdef __DEBUG__
+        screen.text(std::to_string(p.area), minimal_font, Point(p.origin), true, center_center);
+        screen.pen = Pen(255, 255, 255);
+        screen.pixel(p.origin);
+#endif
     }
 
     for(auto &p: particles){
@@ -341,7 +390,8 @@ void render(uint32_t time) {
         screen.line(player1.shot_origin, player1.shot_target);
     }
 
-    /*uint32_t ms_end = now();
+#ifdef __DEBUG__
+    uint32_t ms_end = now();
 
     // draw FPS meter
     screen.alpha = 255;
@@ -355,7 +405,8 @@ void render(uint32_t time) {
     for (uint32_t i = 0; i < (ms_end - ms_start); i++) {
       screen.pen = Pen(i * 5, 255 - (i * 5), 0);
       screen.rectangle(Rect(i * (block_size + 1) + 1 + 13, screen.bounds.h - block_size - 1, block_size, block_size));
-    }*/
+    }
+#endif
 
     screen.pen = Pen(255, 255, 255);
     screen.text("score: " + std::to_string(player1.score) + " lives: " + std::to_string(player1.lives), minimal_font, Point(5, 5));
@@ -373,16 +424,17 @@ void update(uint32_t time) {
 
     if (pressed(Button::DPAD_LEFT))  { player1.rotational_velocity += pi / 720; }
     if (pressed(Button::DPAD_RIGHT)) { player1.rotational_velocity -= pi / 720; }
-    player1.rotational_velocity -= joystick.x * pi / 720;
+    //player1.rotational_velocity -= joystick.x * pi / 720;
 
     if(player1.energy >= MOVEMENT_COST) {
         if (pressed(Button::DPAD_UP))    { movement.y -= 0.03f; }
         if (pressed(Button::DPAD_DOWN))  { movement.y += 0.03f; }
-        movement.y += joystick.y / 10.0f;
+        //movement.y += joystick.y / 10.0f;
 
         if(pressed(Button::DPAD_UP) || pressed(Button::DPAD_DOWN)) {
             player1.energy -= MOVEMENT_COST;
             channels[0].trigger_attack();
+            //thrust(player1.position, player1.velocity);
         } else {
             channels[0].trigger_release();
         }
@@ -398,9 +450,13 @@ void update(uint32_t time) {
     if (buttons.pressed & Button::A && time - player1.t_shot_fired > 50 && player1.energy >= LASER_COST) {
         player1.shot_fired = true;
         player1.t_shot_fired = time;
+
+        // Offet the shot to just in front of the players nose
         Vec2 shot_offset(0.0f, -10.0f);
         shot_offset.rotate(-player1.rotation);
         player1.shot_origin = player1.position + shot_offset;
+
+        // Create a line extrued out from the shot origin
         Vec2 beam(0, 0);
         beam -= Vec2(0, 300);
         beam.rotate(-player1.rotation);
@@ -431,16 +487,17 @@ void update(uint32_t time) {
 
     std::vector<polygon> new_polygons;
 
-    bool player_collides = false;
+    bool player_inside_asteroid = false;
 
     for(auto &p: polygons) {
-
         bool do_split = false;
-        Vec2 last_point = p.points.back();
         uint32_t player_collissions = 0;
+        Vec2 last_point = p.points.back();
         for(auto &point: p.points){
             Vec2 nope;
-            if(!player1.invincible && !player_collides && line_segment_intersection(&nope, last_point, point, player1.position, player1.position + Vec2(1000.0f, player1.position.y))){
+            // Count the number of time a line projected out to the right of the player's origin
+            // collides with the line segment of this polygon
+            if(!player1.invincible && !player_inside_asteroid && line_segment_intersection(&nope, last_point, point, player1.position, player1.position + Vec2(1000.0f, player1.position.y))){
                 player_collissions++;
             }
 
@@ -450,10 +507,12 @@ void update(uint32_t time) {
 
             last_point = point;
         }
+        // If the projected player line collides an *odd* number of times then we know the player is inside a polygon
         if(player_collissions & 0b1) {
-            player_collides = true;
+            player_inside_asteroid = true;
         }
 
+        // If the player's shot intersects any line in this polygon we must slice it into twos
         if(do_split){
             polygon poly = split_polygon(&p, player1.shot_origin, player1.shot_target);
             if(poly.points.size()) {
@@ -463,7 +522,7 @@ void update(uint32_t time) {
     }
     player1.shot_fired = false;
 
-    if(player_collides) {
+    if(player_inside_asteroid) {
         player1.energy -= DAMAGE_COST;
     }
 
@@ -471,10 +530,12 @@ void update(uint32_t time) {
         polygons.push_back(polygon);
     }
 
+    new_polygons.clear();
     polygons.erase(std::remove_if(polygons.begin(), polygons.end(), prune_polygons), polygons.end());
 
     for(auto &polygon: polygons) {
-        polygon.rotational_velocity *= 0.999f;
+        polygon.rotational_velocity *= 0.9999f;
+        polygon.velocity *= 0.9999f;
         rotate_polygon(polygon.points, polygon.rotational_velocity, polygon.origin);
         translate_polygon(polygon, polygon.velocity);
 
@@ -508,8 +569,8 @@ void update(uint32_t time) {
     Vec2 offset(0, 0);
 
     std::vector<Vec2> player1_shape(player1.shape);
-    rotate_polygon(player1_shape, player1.rotation);
     translate_polygon(player1_shape, player1.position);
+    rotate_polygon(player1_shape, player1.rotation, player1.position);
 
     for(auto &p: player1_shape) {
         if(p.x > screen.bounds.w - 1) {
@@ -537,19 +598,11 @@ void update(uint32_t time) {
         explode(player1.position);
         if(player1.lives > 0) {
             player1.lives--;
-        } else {
-            player1.score = 0;
-            player1.lives = STARTING_LIVES;
         }
-        player1.energy = STARTING_ENERGY;
-        player1.position = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
-        player1.velocity = Vec2(0, 0);
-        player1.rotation = 0;
-        player1.rotational_velocity = 0;
-        player1.invincible = true;
+        player1.reset_or_die();
     }
 
     if(player1.energy <= STARTING_ENERGY) {
-        player1.energy += 1;
+        player1.energy += REGEN_ENERGY;
     }
 }
