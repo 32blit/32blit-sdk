2ac148439a121c94780e2a58ebf1118483b858b7
Phil Howard <phil@gadgetoid.com>
2021-08-10 18:03:52 +0100

    Make geometry into a game
    * Add crude sound effects
    * Add score and lives
    * Add energy bar, movement/shooting costs energy
    * Add collission with asteroids, damage costs energ
    * Damage to 0 energy loses a life
    * Invincible after losing a life until 1st shot
    * Score/lives reset after 0

--- examples/geometry/geometry.cpp
+++ examples/geometry/geometry.cpp
@@ -1,14 +1,21 @@
 #include "geometry.hpp"
 #include "graphics/color.hpp"
 #include "types/vec2.hpp"
+#include "engine/particle.hpp"
 
 using namespace blit;
 
+#define __AUDIO__
+
 #define COLLECT_THRESHOLD (unsigned short)150
 #define DISCARD_THRESHOLD (unsigned short)20
-#define STARTING_ENERGY (unsigned short)1000
-#define LASER_COST (unsigned short)10
-#define MOVEMENT_COST (float)0.01
+
+const uint32_t STARTING_ENERGY = 500;
+const uint32_t LASER_COST = 100;
+const uint32_t MOVEMENT_COST = 5;
+const uint32_t DAMAGE_COST = 10;
+const uint8_t STARTING_LIVES = 3;
+const uint8_t P_MAX_AGE = 255;
 
 #define ASTEROID_COUNT (unsigned short)8
 #define ASTEROID_MIN_SIZE (unsigned short)4
@@ -16,6 +23,14 @@ using namespace blit;
 
 #define POLYGON_COUNT (unsigned int)10
 
+struct SpaceDust {
+    blit::Vec2 pos;
+    blit::Vec2 vel;
+    uint32_t age = 0;
+    uint8_t color = 0;
+
+    SpaceDust(Vec2 pos, Vec2 vel, uint8_t color) : pos(pos), vel(vel), color(color) {};
+};
 
 struct player {
     Vec2 velocity;
@@ -23,12 +38,14 @@ struct player {
     float rotation = 0;
     float rotational_velocity = 0;
     std::vector<Vec2> shape;
-    float energy = 0;
+    int32_t energy = 0;
     unsigned int score = 0;
     bool shot_fired = false;
     unsigned int t_shot_fired = 0;
     Vec2 shot_origin;
     Vec2 shot_target;
+    uint8_t lives = 3;
+    bool invincible = false;
 };
 
 struct polygon {
@@ -40,9 +57,35 @@ struct polygon {
     bool prune = false;
 };
 
+std::vector<SpaceDust> particles;
 std::vector<polygon> polygons;
 
 player player1;
+
+void explode(Vec2 origin) {
+    channels[2].frequency = 800;
+    channels[2].trigger_attack();
+    uint8_t count = 5 + (blit::random() % 25);
+    for(auto x = 0u; x < count; x++) {
+        float r = blit::random() % 360;
+        float v = (50.0f + (blit::random() % 100)) / 100.0f;
+        float c = 100 + blit::random() % 128;
+        r  = r * pi / 180.0f;
+
+        particles.push_back(SpaceDust(
+                origin,
+                Vec2(cosf(r) * v, sinf(r) * v),
+                c
+        ));
+    }
+}
+
+bool prune_polygons(polygon p){
+    if(p.prune) {
+        explode(p.origin);
+    }
+    return p.prune;
+}
 
 Rect bounds_of_polygon(std::vector<Vec2> &points) {
     float xmin=1000000, xmax=-1000000, ymin=1000000, ymax=-1000000;
@@ -114,10 +157,15 @@ polygon split_polygon(polygon *poly, Vec2 a, Vec2 b) {
         poly->rotational_velocity += 0.005f;
 
         Rect bounds = bounds_of_polygon(poly->points);
-        poly->prune = (bounds.w * bounds.h) < 70;
+        if((bounds.w * bounds.h) < 70) {
+            player1.score += bounds.w * bounds.h;
+            poly->prune = true;
+        } else {
+            poly->prune = false;
+        }
 
         bounds = bounds_of_polygon(split_b);
-        if((bounds.w * bounds.h) >= 70){
+        if((bounds.w * bounds.h) >= 70) {
             polygon new_polygon;
             new_polygon.colour_offset = poly->colour_offset;
             new_polygon.velocity = poly->velocity * -1;
@@ -127,7 +175,10 @@ polygon split_polygon(polygon *poly, Vec2 a, Vec2 b) {
             return new_polygon;
         }
         else {
-            // TODO: Add particle effect for destroyed polygon
+            player1.score += bounds.w * bounds.h;
+            // This polygon doesn't ever exist, but since we have the list
+            // of points and its bounds we can emenate an explosion from its center
+            explode(Vec2(bounds.center()));
         }
     }
     
@@ -212,11 +263,40 @@ void init() {
         polygons.push_back(p);
     }
 
+    player1.energy = STARTING_ENERGY;
+    player1.lives = STARTING_LIVES;
+    player1.invincible = true;
     player1.position = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
     player1.shape.push_back(Vec2(0, -6));
     player1.shape.push_back(Vec2(-6, 6));
     player1.shape.push_back(Vec2(0, 2));
     player1.shape.push_back(Vec2(6, 6));
+
+#ifdef __AUDIO__
+    channels[0].waveforms   = Waveform::NOISE;
+    channels[0].frequency   = 4200;
+    channels[0].attack_ms   = 10;
+    channels[0].decay_ms    = 1;
+    channels[0].sustain     = 0xffff;
+    channels[0].release_ms  = 10;
+    channels[0].volume      = 4000;
+
+    channels[1].waveforms   = Waveform::SINE;
+    channels[1].frequency   = 0;
+    channels[1].attack_ms   = 10;
+    channels[1].decay_ms    = 500;
+    channels[1].sustain     = 0;
+    channels[1].release_ms  = 0;
+    channels[1].volume      = 3000;
+
+    channels[2].waveforms   = Waveform::NOISE;
+    channels[2].frequency   = 800;
+    channels[2].attack_ms   = 10;
+    channels[2].decay_ms    = 500;
+    channels[2].sustain     = 0;
+    channels[2].release_ms  = 0;
+    channels[2].volume      = 8000;
+#endif
 }
 
 void render(uint32_t time) {
@@ -226,7 +306,13 @@ void render(uint32_t time) {
     screen.pen = Pen(0, 0, 0);
     screen.clear();
 
-    screen.pen = Pen(255, 255, 255);
+    if(player1.invincible) {
+        uint8_t rgb = (sinf(time / 200.0f) * 70) + 100;
+        screen.pen = Pen(rgb, rgb, rgb);
+    }
+    else {
+        screen.pen = Pen(255, 255, 255);
+    }
     std::vector<Vec2> player1_shape(player1.shape);
     rotate_polygon(player1_shape, player1.rotation);
     translate_polygon(player1_shape, player1.position);
@@ -239,13 +325,23 @@ void render(uint32_t time) {
         //screen.pixel(p.origin);
     }
 
+    for(auto &p: particles){
+        screen.pen = Pen(p.color, p.color, p.color, P_MAX_AGE - (uint8_t)p.age);
+        p.age += 2;
+        p.pos += p.vel;
+        screen.pixel(p.pos);
+    }
+
+    particles.erase(std::remove_if(particles.begin(), particles.end(), [](SpaceDust particle) { return (particle.age >= P_MAX_AGE); }), particles.end());
+
+
     if(time - player1.t_shot_fired > 0 && time - player1.t_shot_fired < 500){
         int c = 255 - ((time - player1.t_shot_fired ) / 2);
         screen.pen = Pen(c, c, c);
         screen.line(player1.shot_origin, player1.shot_target);
     }
 
-    uint32_t ms_end = now();
+    /*uint32_t ms_end = now();
 
     // draw FPS meter
     screen.alpha = 255;
@@ -259,36 +355,70 @@ void render(uint32_t time) {
     for (uint32_t i = 0; i < (ms_end - ms_start); i++) {
       screen.pen = Pen(i * 5, 255 - (i * 5), 0);
       screen.rectangle(Rect(i * (block_size + 1) + 1 + 13, screen.bounds.h - block_size - 1, block_size, block_size));
-    }
+    }*/
+
+    screen.pen = Pen(255, 255, 255);
+    screen.text("score: " + std::to_string(player1.score) + " lives: " + std::to_string(player1.lives), minimal_font, Point(5, 5));
+
+    screen.pen = Pen(0, 255, 0, 200);
+    Rect energy = Rect(5, screen.bounds.h - 5, 0, 5);
+    energy.y -= energy.h;
+    energy.w = player1.energy * (screen.bounds.w - 10) / STARTING_ENERGY;
+    screen.rectangle(energy);
 }
 
-bool prune_polygons(polygon p){
-    // TODO: Add particle effect for destroyed polygons
-    return p.prune;
-}
-
-bool split = false;
 void update(uint32_t time) {
 
     Vec2 movement(0, 0);
 
     if (pressed(Button::DPAD_LEFT))  { player1.rotational_velocity += pi / 720; }
     if (pressed(Button::DPAD_RIGHT)) { player1.rotational_velocity -= pi / 720; }
-    if (pressed(Button::DPAD_UP))    { movement.y -= 0.03f; }
-    if (pressed(Button::DPAD_DOWN))  { movement.y += 0.03f; }
-
     player1.rotational_velocity -= joystick.x * pi / 720;
-    movement.y += joystick.y / 10.0f;
 
-    if (pressed(Button::A) && time - player1.t_shot_fired > 500) {
+    if(player1.energy >= MOVEMENT_COST) {
+        if (pressed(Button::DPAD_UP))    { movement.y -= 0.03f; }
+        if (pressed(Button::DPAD_DOWN))  { movement.y += 0.03f; }
+        movement.y += joystick.y / 10.0f;
+
+        if(pressed(Button::DPAD_UP) || pressed(Button::DPAD_DOWN)) {
+            player1.energy -= MOVEMENT_COST;
+            channels[0].trigger_attack();
+        } else {
+            channels[0].trigger_release();
+        }
+    } else {
+        channels[0].trigger_release();
+    }
+
+    // Player loses invincibility if they shoot
+    if(buttons.pressed & Button::A) {
+        player1.invincible = false;
+    }
+
+    if (buttons.pressed & Button::A && time - player1.t_shot_fired > 50 && player1.energy >= LASER_COST) {
         player1.shot_fired = true;
         player1.t_shot_fired = time;
-        player1.shot_origin = player1.position;
+        Vec2 shot_offset(0.0f, -10.0f);
+        shot_offset.rotate(-player1.rotation);
+        player1.shot_origin = player1.position + shot_offset;
         Vec2 beam(0, 0);
-        beam -= Vec2(0, 200);
+        beam -= Vec2(0, 300);
         beam.rotate(-player1.rotation);
         beam += player1.shot_origin;
         player1.shot_target = Vec2(beam.x, beam.y);
+
+        channels[1].frequency = 2000;
+        channels[1].trigger_attack();
+        player1.energy -= LASER_COST;
+    }
+
+    // Decay the shot and explosion frequency
+    if(channels[1].frequency  > 0) {
+        channels[1].frequency *= 0.98f;
+    }
+
+    if(channels[2].frequency  > 0) {
+        channels[2].frequency *= 0.98f;
     }
 
     movement.rotate(-player1.rotation);
@@ -301,26 +431,40 @@ void update(uint32_t time) {
 
     std::vector<polygon> new_polygons;
 
-    if(player1.shot_fired){
-        for(auto &p: polygons) {
+    bool player_collides = false;
 
-            bool do_split = false;
-            Vec2 last_point = p.points.back();
-            for(auto &point: p.points){
-                Vec2 nope;
-                do_split = line_segment_intersection(&nope, last_point, point, player1.shot_origin, player1.shot_target);
-                if(do_split) break;
-                last_point = point;
+    for(auto &p: polygons) {
+
+        bool do_split = false;
+        Vec2 last_point = p.points.back();
+        uint32_t player_collissions = 0;
+        for(auto &point: p.points){
+            Vec2 nope;
+            if(!player1.invincible && !player_collides && line_segment_intersection(&nope, last_point, point, player1.position, player1.position + Vec2(1000.0f, player1.position.y))){
+                player_collissions++;
             }
 
-            if(do_split){
-                polygon poly = split_polygon(&p, player1.shot_origin, player1.shot_target);
-                if(poly.points.size()) {
-                    new_polygons.push_back(poly);
-                }
+            if(player1.shot_fired && !do_split){
+                do_split = line_segment_intersection(&nope, last_point, point, player1.shot_origin, player1.shot_target);
+            }
+
+            last_point = point;
+        }
+        if(player_collissions & 0b1) {
+            player_collides = true;
+        }
+
+        if(do_split){
+            polygon poly = split_polygon(&p, player1.shot_origin, player1.shot_target);
+            if(poly.points.size()) {
+                new_polygons.push_back(poly);
             }
         }
-        player1.shot_fired = false;
+    }
+    player1.shot_fired = false;
+
+    if(player_collides) {
+        player1.energy -= DAMAGE_COST;
     }
 
     for(auto &polygon: new_polygons){
@@ -388,4 +532,24 @@ void update(uint32_t time) {
     }
 
     player1.position += offset;
+
+    if(player1.energy < 0) {
+        explode(player1.position);
+        if(player1.lives > 0) {
+            player1.lives--;
+        } else {
+            player1.score = 0;
+            player1.lives = STARTING_LIVES;
+        }
+        player1.energy = STARTING_ENERGY;
+        player1.position = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
+        player1.velocity = Vec2(0, 0);
+        player1.rotation = 0;
+        player1.rotational_velocity = 0;
+        player1.invincible = true;
+    }
+
+    if(player1.energy <= STARTING_ENERGY) {
+        player1.energy += 1;
+    }
 }
