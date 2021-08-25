7cc98872794d6c750d988c0dfd644ca9e001c66e
Charlie Birks <charlie@daft.games>
2021-08-25 12:06:37 +0100

    Fix hardcoded video size

--- 32blit-sdl/VideoCaptureFfmpeg.cpp
+++ 32blit-sdl/VideoCaptureFfmpeg.cpp
@@ -523,7 +523,7 @@ int ffmpeg_open_stream(const char *filename, int width, int height, uint8_t *pic
 	/* Add the audio and video streams using the default format codecs
 	 * and initialize the codecs. */
 	if (fmt->video_codec != AV_CODEC_ID_NONE) {
-		add_stream(&video_st, oc, &video_codec, fmt->video_codec, 640, 480);
+		add_stream(&video_st, oc, &video_codec, fmt->video_codec, width * 2, height * 2);
 		have_video = 1;
 		encode_video = 1;
 	}
