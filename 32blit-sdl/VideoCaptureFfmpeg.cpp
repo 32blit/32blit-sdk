6302a04139128330724ec9af55f8db8601667432
Charlie Birks <charlie@daft.games>
2021-08-25 12:06:11 +0100

    Remove dummy image creation from video capture

--- 32blit-sdl/VideoCaptureFfmpeg.cpp
+++ 32blit-sdl/VideoCaptureFfmpeg.cpp
@@ -406,35 +406,6 @@ static void open_video(AVFormatContext *oc, AVCodec *codec, OutputStream *ost, A
 		exit(1);
 	}
 }
-/* Prepare a dummy image. */
-static void fill_yuv_image(AVFrame *pict, int frame_index,
-	int width, int height)
-{
-	int x, y, i;
-	i = frame_index;
-	/* Y */
-	for (y = 0; y < height; y++)
-		for (x = 0; x < width; x++)
-			pict->data[0][y * pict->linesize[0] + x] = x + y + i * 3;
-	/* Cb and Cr */
-	for (y = 0; y < height / 2; y++) {
-		for (x = 0; x < width / 2; x++) {
-			pict->data[1][y * pict->linesize[1] + x] = 128 + y + i * 2;
-			pict->data[2][y * pict->linesize[2] + x] = 64 + x + i * 5;
-		}
-	}
-}
-static void fill_rgb_image(AVFrame *pict, int frame_index,
-	int width, int height) {
-	srand(frame_index);
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			pict->data[0][y * pict->linesize[0] + (x * 3) + 0] = rand() % 128;
-			pict->data[0][y * pict->linesize[0] + (x * 3) + 1] = rand() % 255;
-			pict->data[0][y * pict->linesize[0] + (x * 3) + 2] = rand() % 128;
-		}
-	}
-}
 
 static void fill_from_source(AVFrame *pict, int frame_index, int width, int height) {
 	if (source_format == AV_PIX_FMT_RGB24){
