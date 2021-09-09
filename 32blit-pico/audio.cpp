e4debd38bccd805b0c4565de7a56113d0ac17fb3
Phil Howard <phil@gadgetoid.com>
2021-09-09 09:33:18 +0100

    pico: PWM audio support for PicoSystem
    Bring up PWM audio on GPIO 11, pio1 for PicoSystem.
    
    This enables audio output for the  32blit audio engine,
    but the transistor-amplified piezo on PicoSystem is *really* quiet.
    
    Music is probably a no-go on this feeble beeper.

--- 32blit-pico/audio.cpp
+++ 32blit-pico/audio.cpp
@@ -3,6 +3,14 @@
 #ifdef AUDIO_I2S
 #include "pico/audio_i2s.h"
 #define HAVE_AUDIO
+#define AUDIO_SAMPLE_FREQ 44100
+#endif
+
+#ifdef AUDIO_PWM
+#include "pico/audio_pwm.h"
+#include "hardware/pio.h"
+#define HAVE_AUDIO
+#define AUDIO_SAMPLE_FREQ 22050
 #endif
 
 #include "audio/audio.hpp"
@@ -12,9 +20,9 @@ static audio_buffer_pool *audio_pool = nullptr;
 #endif
 
 void init_audio() {
-#ifdef AUDIO_I2S
+#ifdef HAVE_AUDIO
   static audio_format_t audio_format = {
-    .sample_freq = 44100,
+    .sample_freq = AUDIO_SAMPLE_FREQ,
     .format = AUDIO_BUFFER_FORMAT_PCM_S16,
     .channel_count = 1
   };
@@ -27,6 +35,7 @@ void init_audio() {
   struct audio_buffer_pool *producer_pool = audio_new_producer_pool(&producer_format, 4, 441);
   const struct audio_format *output_format;
 
+#ifdef AUDIO_I2S
   struct audio_i2s_config config = {
     .data_pin = PICO_AUDIO_I2S_DATA_PIN,
     .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
@@ -42,6 +51,29 @@ void init_audio() {
   bool ok = audio_i2s_connect(producer_pool);
   assert(ok);
   audio_i2s_set_enabled(true);
+#endif
+
+#ifdef AUDIO_PWM
+  struct audio_pwm_channel_config audio_pwm_config = {
+    .core = {
+      .base_pin = PICO_AUDIO_PWM_MONO_PIN,
+      .dma_channel = 1,
+      .pio_sm = 1,
+    },
+    .pattern = 3,
+  };
+  output_format = audio_pwm_setup(&audio_format, -1, &audio_pwm_config);
+  if (!output_format) {
+      panic("PicoAudio: Unable to open audio device.\n");
+  }
+  pio_sm_set_clkdiv(pio1, 1, 2.0f); //!
+  bool ok = audio_pwm_default_connect(producer_pool, false);
+  assert(ok);
+  audio_pwm_set_enabled(true);
+  gpio_set_drive_strength(PICO_AUDIO_PWM_MONO_PIN, GPIO_DRIVE_STRENGTH_4MA);
+  gpio_set_slew_rate(PICO_AUDIO_PWM_MONO_PIN, GPIO_SLEW_RATE_FAST);
+#endif
+
   audio_pool = producer_pool;
 #endif
 }
@@ -52,12 +84,19 @@ void update_audio() {
   struct audio_buffer *buffer = take_audio_buffer(audio_pool, false);
   if(buffer) {
     auto samples = (int16_t *) buffer->buffer->bytes;
+#ifdef AUDIO_I2S
     for(uint32_t i = 0; i < buffer->max_sample_count; i += 2) {
       int val = (int)blit::get_audio_frame() - 0x8000;
       *samples++ = val;
       *samples++ = val;
     }
-
+#endif
+#ifdef AUDIO_PWM
+    for(uint32_t i = 0; i < buffer->max_sample_count; i++) {
+      int val = (int)blit::get_audio_frame() - 0x8000;
+      *samples++ = val;
+    }
+#endif
     buffer->sample_count = buffer->max_sample_count;
     give_audio_buffer(audio_pool, buffer);
   }
