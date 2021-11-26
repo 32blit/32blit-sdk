bb3a0dd645765b0f4520be9804f8db81b6f557b2
Charlie Birks <charlie@daft.games>
2021-11-26 17:02:15 +0000

    Move SpriteTransform to surface.hpp

--- 32blit/graphics/sprite.hpp
+++ 32blit/graphics/sprite.hpp
@@ -4,26 +4,4 @@
 
 namespace blit {
   using SpriteSheet [[deprecated]] = Surface;
-
-  /** 
-   * All sprite mirroring and rotations (90/180/270) can be composed
-   * of simple horizontal/vertical flips and x/y coordinate swaps.
-   *
-   * The bits set represent the transforms required to achieve the
-   * end result. Operations are performed (if needed) in the following 
-   * order: horizontal flip -> vertical flip -> x/y swap
-   * 
-   * For example a 90 degree rotation needs a vertical flip
-   * followed by an x/y coordinate swap.
-  */
-  enum SpriteTransform {
-    NONE = 0b000,
-    HORIZONTAL = 0b001,
-    VERTICAL = 0b010,
-    XYSWAP = 0b100,
-    R90 = 0b101,
-    R180 = 0b011,
-    R270 = 0b110
-  };
-
-} 
+}
--- 32blit/graphics/surface.hpp
+++ 32blit/graphics/surface.hpp
@@ -19,7 +19,26 @@
 
 namespace blit {
 
-  struct sprite_p;
+  /**
+   * All sprite mirroring and rotations (90/180/270) can be composed
+   * of simple horizontal/vertical flips and x/y coordinate swaps.
+   *
+   * The bits set represent the transforms required to achieve the
+   * end result. Operations are performed (if needed) in the following
+   * order: horizontal flip -> vertical flip -> x/y swap
+   *
+   * For example a 90 degree rotation needs a vertical flip
+   * followed by an x/y coordinate swap.
+  */
+  enum SpriteTransform {
+    NONE = 0b000,
+    HORIZONTAL = 0b001,
+    VERTICAL = 0b010,
+    XYSWAP = 0b100,
+    R90 = 0b101,
+    R180 = 0b011,
+    R270 = 0b110
+  };
 
 #pragma pack(push, 1)
   struct packed_image {
