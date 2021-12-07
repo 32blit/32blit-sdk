c95c82d2c9ea7bb55d988d1246ba79825b3a6b3a
Charlie Birks <charlie@daft.games>
2021-12-07 16:05:59 +0000

    Add some "fast code" macros
    Inspired by the "not in flash" macros from the pico sdk (and the pico support uses those)

---
+++ 32blit/engine/fast_code.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef TARGET_32BLIT_HW
+#define blit_fast_code(fn) __attribute__((section(".itcm." #fn))) fn
+#define blit_no_inline_fast_code(fn) __attribute__((noinline)) blit_fast_code(fn)
+#elif defined(PICO_BUILD)
+#include <pico/platform.h>
+
+#define blit_fast_code(fn) __not_in_flash_func(fn)
+#define blit_no_inline_fast_code(fn) __no_inline_not_in_flash_func(fn)
+#else
+#define blit_fast_code(fn) fn
+#define blit_no_inline_fast_code(fn) fn
+#endif
