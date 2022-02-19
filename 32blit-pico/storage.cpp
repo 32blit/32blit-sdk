f3c74bf2b965542044bc14a9742a9daa7340858c
Charlie Birks <charlie@daft.games>
2022-02-19 19:46:53 +0000

    pico: Use the tag define in storage.cpp

--- 32blit-pico/storage.cpp
+++ 32blit-pico/storage.cpp
@@ -7,6 +7,8 @@
 #include "hardware/sync.h"
 #include "pico/binary_info.h"
 
+#include "binary_info.hpp"
+
 static const uint32_t storage_size = PICO_FLASH_SIZE_BYTES / 4;
 static const uint32_t storage_offset = PICO_FLASH_SIZE_BYTES - storage_size;
 
@@ -15,7 +17,7 @@ void get_storage_size(uint16_t &block_size, uint32_t &num_blocks) {
   num_blocks = storage_size / FLASH_SECTOR_SIZE;
 
   bi_decl(bi_block_device(
-    BINARY_INFO_MAKE_TAG('3', 'B'),
+    BINARY_INFO_TAG_32BLIT,
     "32Blit",
     XIP_BASE + storage_offset,
     storage_size,
