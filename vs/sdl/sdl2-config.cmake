d91af62487f7dd5afc78fd6d80ea7c3825464e90
Charlie Birks <charlie@daft.games>
2022-09-08 14:50:21 +0100

    Deduplicate SDL versions

--- vs/sdl/sdl2-config.cmake
+++ vs/sdl/sdl2-config.cmake
@@ -17,35 +17,38 @@ function(fetch_sdl2_library directory url filename hash)
     # file(ARCHIVE_EXTRACT INPUT ${filename})
 endfunction(fetch_sdl2_library)
 
+set(SDL2_VERSION 2.24.0)
+set(SDL2_IMAGE_VERSION 2.6.2)
+set(SDL2_NET_VERSION 2.2.0)
 
 if(NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/include)
 
     fetch_sdl2_library(
-        SDL2-2.24.0
-        https://github.com/libsdl-org/SDL/releases/download/release-2.24.0/
-        SDL2-devel-2.24.0-VC.zip
+        SDL2-${SDL2_VERSION}
+        https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/
+        SDL2-devel-${SDL2_VERSION}-VC.zip
         8a54459189e88c30ba024ee5f18ce4b1a5d1d9e7
     )
 
     fetch_sdl2_library(
-        SDL2_image-2.6.2
-        https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.2/
-        SDL2_image-devel-2.6.2-VC.zip
+        SDL2_image-${SDL2_IMAGE_VERSION}
+        https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL2_IMAGE_VERSION}/
+        SDL2_image-devel-${SDL2_IMAGE_VERSION}-VC.zip
         4111affcca1f4b41c2f4b4c445ccf06fe081b5e9
     )
 
     fetch_sdl2_library(
-        SDL2_net-2.2.0
-        https://github.com/libsdl-org/SDL_net/releases/download/release-2.2.0/
-        SDL2_net-devel-2.2.0-VC.zip
+        SDL2_net-${SDL2_NET_VERSION}
+        https://github.com/libsdl-org/SDL_net/releases/download/release-${SDL2_NET_VERSION}/
+        SDL2_net-devel-${SDL2_NET_VERSION}-VC.zip
         c8ff358a5c8338002b05ab6de7ce91ee1c86bd45
     )
 
 endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/SDL2-2.24.0/cmake/sdl2-config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/SDL2-${SDL2_VERSION}/cmake/sdl2-config.cmake)
 get_property(SDL2_DLL TARGET SDL2::SDL2 PROPERTY IMPORTED_LOCATION)
 
 # help cmake to find the other libs
-set(SDL2_image_DIR ${CMAKE_CURRENT_LIST_DIR}/SDL2_image-2.6.2/cmake)
-set(SDL2_net_DIR ${CMAKE_CURRENT_LIST_DIR}/SDL2_net-2.2.0/cmake)
+set(SDL2_image_DIR ${CMAKE_CURRENT_LIST_DIR}/SDL2_image-${SDL2_IMAGE_VERSION}/cmake)
+set(SDL2_net_DIR ${CMAKE_CURRENT_LIST_DIR}/SDL2_net-${SDL2_NET_VERSION}/cmake)
