b45301c54bfd0993fe9c91d8fa2536d0592d401d
Charlie Birks <charlie@daft.games>
2023-01-30 11:51:16 +0000

    pico: move st7789 define from board config

--- 32blit-pico/board/pimoroni_picosystem/config.cmake
+++ 32blit-pico/board/pimoroni_picosystem/config.cmake
@@ -2,7 +2,6 @@ set(BLIT_BOARD_NAME "PicoSystem")
 
 set(BLIT_BOARD_DEFINITIONS
     AUDIO_BEEP_PIN=PICOSYSTEM_AUDIO_PIN
-    DISPLAY_ST7789 # code in both the sdk and some games still depends on this
     INPUT_GPIO
     LED_R_PIN=PICOSYSTEM_LED_R_PIN
     LED_G_PIN=PICOSYSTEM_LED_G_PIN
