ec03678ca20700d917806b841233cbe75b4c6774
Charlie Birks <charlie@daft.games>
2023-07-13 20:42:04 +0100

    pico: move display pack definitions to config.h

--- 32blit-pico/board/display_pack/config.cmake
+++ 32blit-pico/board/display_pack/config.cmake
@@ -1,13 +1,4 @@
 set(BLIT_BOARD_NAME "Display Pack")
 
-set(BLIT_BOARD_DEFINITIONS
-    DISPLAY_HEIGHT=135
-
-    LED_INVERTED
-    LED_R_PIN=6
-    LED_G_PIN=7
-    LED_B_PIN=8
-)
-
 blit_driver(display st7789)
 blit_driver(input gpio_abxy)
--- 32blit-pico/board/display_pack_2/config.cmake
+++ 32blit-pico/board/display_pack_2/config.cmake
@@ -1,13 +1,4 @@
 set(BLIT_BOARD_NAME "Display Pack 2.0")
 
-set(BLIT_BOARD_DEFINITIONS
-    DISPLAY_WIDTH=320
-
-    LED_INVERTED
-    LED_R_PIN=6
-    LED_G_PIN=7
-    LED_B_PIN=8
-)
-
 blit_driver(display st7789)
 blit_driver(input gpio_abxy)
