d2a52bd47f75f00df9ac7f56ff7bb669a5cd01e1
Charlie Birks <charlie@daft.games>
2023-11-19 15:05:59 +0000

    stm32: minimally handle bounds in set_screen_mode_format

--- 32blit-stm32/Src/display.cpp
+++ 32blit-stm32/Src/display.cpp
@@ -145,13 +145,21 @@ namespace display {
 
     switch(new_mode) {
       case ScreenMode::lores:
-        new_surf_template.bounds = lores_screen_size;
+        if(new_surf_template.bounds.empty())
+          new_surf_template.bounds = lores_screen_size;
+        else
+          new_surf_template.bounds /= 2;
         break;
       case ScreenMode::hires:
       case ScreenMode::hires_palette:
-        new_surf_template.bounds = hires_screen_size;
+        if(new_surf_template.bounds.empty())
+          new_surf_template.bounds = hires_screen_size;
         break;
     }
+
+    // only two resolutions supported
+    if(new_surf_template.bounds != lores_screen_size && new_surf_template.bounds != hires_screen_size)
+      return false;
 
     switch(new_surf_template.format) {
       case PixelFormat::RGB:
