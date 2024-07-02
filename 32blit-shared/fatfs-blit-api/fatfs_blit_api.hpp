8068233ceae3554f4bf4f0a3d7e33cf8d4bac5e6
Charlie Birks <charlie@daft.games>
2024-07-02 12:57:07 +0100

    Split a copy of (most of) 32blit-pico's fatfs glue to a library

---
+++ 32blit-shared/fatfs-blit-api/fatfs_blit_api.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "engine/file.hpp"
+
+bool get_files_open();
+void close_open_files();
+
+void *open_file(const std::string &file, int mode);
+int32_t read_file(void *fh, uint32_t offset, uint32_t length, char *buffer);
+int32_t write_file(void *fh, uint32_t offset, uint32_t length, const char *buffer);
+int32_t close_file(void *fh);
+uint32_t get_file_length(void *fh);
+void list_files(const std::string &path, std::function<void(blit::FileInfo &)> callback);
+bool file_exists(const std::string &path);
+bool directory_exists(const std::string &path);
+bool create_directory(const std::string &path);
+bool rename_file(const std::string &old_name, const std::string &new_name);
+bool remove_file(const std::string &path);
