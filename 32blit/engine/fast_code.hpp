efef9de4f54034e4a787d65d5adfaa58a42e879e
Charlie Birks <charlie@daft.games>
2024-08-10 15:14:21 +0100

    Include pico.h for "fast code"
    pico/platform.h is not allowed now

--- 32blit/engine/fast_code.hpp
+++ 32blit/engine/fast_code.hpp
@@ -4,7 +4,7 @@
 #define blit_fast_code(fn) __attribute__((section(".itcm." #fn))) fn
 #define blit_no_inline_fast_code(fn) __attribute__((noinline)) blit_fast_code(fn)
 #elif defined(PICO_BUILD)
-#include <pico/platform.h>
+#include <pico.h>
 
 #define blit_fast_code(fn) __not_in_flash_func(fn)
 #define blit_no_inline_fast_code(fn) __no_inline_not_in_flash_func(fn)
