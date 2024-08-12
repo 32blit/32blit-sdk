1a7f4a1d8b6236f48918be63f7226cee96c9d05d
Charlie Birks <charlie@daft.games>
2024-08-12 22:23:36 +0100

    pico: shuffle code around, remove unused (defaulted) arguments

--- 32blit-pico/display/st7789.cpp
+++ 32blit-pico/display/st7789.cpp
@@ -99,10 +99,6 @@ static uint16_t *frame_buffer = nullptr;
 // pixel double scanline counter
 static volatile int cur_scanline = 240;
 
-static void command(uint8_t command, size_t len = 0, const char *data = NULL);
-static void prepare_write();
-static void set_window(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
-
 // PIO helpers
 static void pio_put_byte(PIO pio, uint sm, uint8_t b) {
   while (pio_sm_is_tx_fifo_full(pio, sm));
@@ -130,7 +126,234 @@ static void __isr st7789_dma_irq_handler() {
   }
 }
 
-static void init(bool auto_init_sequence = true) {
+static void command(uint8_t command, size_t len = 0, const char *data = nullptr) {
+  pio_wait(pio, pio_sm);
+
+  if(write_mode) {
+    // reconfigure to 8 bits
+    pio_sm_set_enabled(pio, pio_sm, false);
+    pio->sm[pio_sm].shiftctrl &= ~PIO_SM0_SHIFTCTRL_PULL_THRESH_BITS;
+    pio->sm[pio_sm].shiftctrl |= (8 << PIO_SM0_SHIFTCTRL_PULL_THRESH_LSB) | PIO_SM0_SHIFTCTRL_AUTOPULL_BITS;
+
+    // switch back to raw
+    pio_sm_restart(pio, pio_sm);
+    pio_sm_set_wrap(pio, pio_sm, pio_offset + st7789_raw_wrap_target, pio_offset + st7789_raw_wrap);
+    pio_sm_exec(pio, pio_sm, pio_encode_jmp(pio_offset));
+
+    pio_sm_set_enabled(pio, pio_sm, true);
+    write_mode = false;
+  }
+
+  gpio_put(LCD_CS_PIN, 0);
+
+  gpio_put(LCD_DC_PIN, 0); // command mode
+  pio_put_byte(pio, pio_sm, command);
+
+  if(data) {
+    pio_wait(pio, pio_sm);
+    gpio_put(LCD_DC_PIN, 1); // data mode
+
+    for(size_t i = 0; i < len; i++)
+      pio_put_byte(pio, pio_sm, data[i]);
+  }
+
+  pio_wait(pio, pio_sm);
+  gpio_put(LCD_CS_PIN, 1);
+}
+
+static void set_window(uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
+  uint32_t cols = __builtin_bswap32((x << 16) | (x + w - 1));
+  uint32_t rows = __builtin_bswap32((y << 16) | (y + h - 1));
+
+  command(reg::CASET, 4, (const char *)&cols);
+  command(reg::RASET, 4, (const char *)&rows);
+
+  win_w = w;
+  win_h = h;
+}
+
+static void st7789_init_sequence() {
+  command(reg::SWRESET);
+
+  sleep_ms(150);
+
+  command(reg::TEON,      1, "\x00");  // enable frame sync signal if used
+  command(reg::COLMOD,    1, "\x05");  // 16 bits per pixel
+
+  if(width == 240 && height == 240) {
+    command(reg::PORCTRL, 5, "\x0c\x0c\x00\x33\x33");
+    command(reg::GCTRL, 1, "\x14");
+    command(reg::VCOMS, 1, "\x37");
+    command(reg::LCMCTRL, 1, "\x2c");
+    command(reg::VDVVRHEN, 1, "\x01");
+    command(reg::VRHS, 1, "\x12");
+    command(reg::VDVS, 1, "\x20");
+    command(reg::PWCTRL1, 2, "\xa4\xa1");
+
+    command(reg::PVGAMCTRL, 14, "\xD0\x08\x11\x08\x0c\x15\x39\x33\x50\x36\x13\x14\x29\x2d");
+    command(reg::NVGAMCTRL, 14, "\xD0\x08\x10\x08\x06\x06\x39\x44\x51\x0b\x16\x14\x2f\x31");
+
+
+    // trigger "vsync" slightly earlier to avoid tearing while pixel-doubling
+    // (this is still outside of the visible part of the screen)
+    command(reg::STE, 2, "\x01\x2C");
+  }
+
+  if(width == 320 && height == 240) {
+    command(reg::PORCTRL, 5, "\x0c\x0c\x00\x33\x33");
+    command(reg::GCTRL, 1, "\x35");
+    command(reg::VCOMS, 1, "\x1f");
+    command(reg::LCMCTRL, 1, "\x2c");
+    command(reg::VDVVRHEN, 1, "\x01");
+    command(reg::VRHS, 1, "\x12");
+    command(reg::VDVS, 1, "\x20");
+    command(reg::PWCTRL1, 2, "\xa4\xa1");
+    command(0xd6, 1, "\xa1"); // ???
+    command(reg::PVGAMCTRL, 14, "\xD0\x08\x11\x08\x0C\x15\x39\x33\x50\x36\x13\x14\x29\x2D");
+    command(reg::NVGAMCTRL, 14, "\xD0\x08\x10\x08\x06\x06\x39\x44\x51\x0B\x16\x14\x2F\x31");
+  }
+
+  command(reg::FRCTRL2, 1, "\x15"); // 50Hz
+
+  command(reg::INVON);   // set inversion mode
+  command(reg::SLPOUT);  // leave sleep mode
+  command(reg::DISPON);  // turn display on
+
+  sleep_ms(100);
+
+  // setup correct addressing window
+  uint8_t madctl = MADCTL::RGB | rotations[LCD_ROTATION / 90];
+  if(width == 240 && height == 240) {
+    set_window(0, 0, 240, 240);
+  }
+
+  if(width == 240 && height == 135) {
+    set_window(40, 53, 240, 135);
+  }
+
+  if(width == 320 && height == 240) {
+    set_window(0, 0, 320, 240);
+  }
+
+  command(reg::MADCTL,    1, (char *)&madctl);
+}
+
+static void prepare_write() {
+  pio_wait(pio, pio_sm);
+
+  // setup for writing
+  uint8_t r = reg::RAMWR;
+  gpio_put(LCD_CS_PIN, 0);
+
+  gpio_put(LCD_DC_PIN, 0); // command mode
+  pio_put_byte(pio, pio_sm, r);
+  pio_wait(pio, pio_sm);
+
+  gpio_put(LCD_DC_PIN, 1); // data mode
+
+  pio_sm_set_enabled(pio, pio_sm, false);
+  pio_sm_restart(pio, pio_sm);
+
+  if(pixel_double) {
+    // switch program
+    pio_sm_set_wrap(pio, pio_sm, pio_double_offset + st7789_pixel_double_wrap_target, pio_double_offset + st7789_pixel_double_wrap);
+
+    // 32 bits, no autopull
+    pio->sm[pio_sm].shiftctrl &= ~(PIO_SM0_SHIFTCTRL_PULL_THRESH_BITS | PIO_SM0_SHIFTCTRL_AUTOPULL_BITS);
+
+    pio_sm_exec(pio, pio_sm, pio_encode_jmp(pio_double_offset));
+
+    // reconfigure dma size
+    dma_channel_hw_addr(dma_channel)->al1_ctrl &= ~DMA_CH0_CTRL_TRIG_DATA_SIZE_BITS;
+    dma_channel_hw_addr(dma_channel)->al1_ctrl |= DMA_SIZE_32 << DMA_CH0_CTRL_TRIG_DATA_SIZE_LSB;
+  } else {
+    // 16 bits, autopull
+    pio->sm[pio_sm].shiftctrl &= ~PIO_SM0_SHIFTCTRL_PULL_THRESH_BITS;
+    pio->sm[pio_sm].shiftctrl |= (16 << PIO_SM0_SHIFTCTRL_PULL_THRESH_LSB) | PIO_SM0_SHIFTCTRL_AUTOPULL_BITS;
+
+    dma_channel_hw_addr(dma_channel)->al1_ctrl &= ~DMA_CH0_CTRL_TRIG_DATA_SIZE_BITS;
+    dma_channel_hw_addr(dma_channel)->al1_ctrl |= DMA_SIZE_16 << DMA_CH0_CTRL_TRIG_DATA_SIZE_LSB;
+  }
+
+  pio_sm_set_enabled(pio, pio_sm, true);
+
+  write_mode = true;
+}
+
+static void update() {
+  dma_channel_wait_for_finish_blocking(dma_channel);
+
+  if(!write_mode)
+    prepare_write();
+
+  if(pixel_double) {
+    cur_scanline = 0;
+    upd_frame_buffer = frame_buffer;
+    dma_channel_set_trans_count(dma_channel, win_w / 4, false);
+  } else
+    dma_channel_set_trans_count(dma_channel, win_w * win_h, false);
+
+  dma_channel_set_read_addr(dma_channel, frame_buffer, true);
+}
+
+static void set_backlight(uint8_t brightness) {
+#ifdef LCD_BACKLIGHT_PIN
+  // gamma correct the provided 0-255 brightness value onto a
+  // 0-65535 range for the pwm counter
+  float gamma = 2.8;
+  uint16_t value = (uint16_t)(pow((float)(brightness) / 255.0f, gamma) * 65535.0f + 0.5f);
+  pwm_set_gpio_level(LCD_BACKLIGHT_PIN, value);
+#endif
+}
+
+static bool vsync_callback(gpio_irq_callback_t callback) {
+#ifdef LCD_VSYNC_PIN
+  gpio_set_irq_enabled_with_callback(LCD_VSYNC_PIN, GPIO_IRQ_EDGE_RISE, true, callback);
+  return true;
+#else
+  return false;
+#endif
+}
+
+static void set_pixel_double(bool pd) {
+  pixel_double = pd;
+
+  // nop to reconfigure PIO
+  if(write_mode)
+    command(0);
+
+  if(pixel_double) {
+    dma_channel_acknowledge_irq0(dma_channel);
+    dma_channel_set_irq0_enabled(dma_channel, true);
+  } else
+    dma_channel_set_irq0_enabled(dma_channel, false);
+}
+
+static void clear() {
+  if(!write_mode)
+    prepare_write();
+
+  for(int i = 0; i < win_w * win_h; i++)
+    pio_sm_put_blocking(pio, pio_sm, 0);
+}
+
+static bool dma_is_busy() {
+  if(pixel_double && cur_scanline <= win_h / 2)
+    return true;
+
+  return dma_channel_is_busy(dma_channel);
+}
+
+static void vsync_callback(uint gpio, uint32_t events) {
+  if(!do_render && !dma_is_busy()) {
+    ::update();
+    do_render = true;
+  }
+}
+
+void init_display() {
+  frame_buffer = screen_fb;
+
   // configure pins
   gpio_set_function(LCD_DC_PIN, GPIO_FUNC_SIO);
   gpio_set_dir(LCD_DC_PIN, GPIO_OUT);
@@ -225,73 +448,8 @@ static void init(bool auto_init_sequence = true) {
   bi_decl_if_func_used(bi_1pin_with_name(LCD_SCK_PIN, "Display SCK"));
 #endif
 
-  // if auto_init_sequence then send initialisation sequence
-  // for our standard displays based on the width and height
-  if(auto_init_sequence) {
-    command(reg::SWRESET);
-
-    sleep_ms(150);
-
-    command(reg::TEON,      1, "\x00");  // enable frame sync signal if used
-    command(reg::COLMOD,    1, "\x05");  // 16 bits per pixel
-
-    if(width == 240 && height == 240) {
-      command(reg::PORCTRL, 5, "\x0c\x0c\x00\x33\x33");
-      command(reg::GCTRL, 1, "\x14");
-      command(reg::VCOMS, 1, "\x37");
-      command(reg::LCMCTRL, 1, "\x2c");
-      command(reg::VDVVRHEN, 1, "\x01");
-      command(reg::VRHS, 1, "\x12");
-      command(reg::VDVS, 1, "\x20");
-      command(reg::PWCTRL1, 2, "\xa4\xa1");
-
-      command(reg::PVGAMCTRL, 14, "\xD0\x08\x11\x08\x0c\x15\x39\x33\x50\x36\x13\x14\x29\x2d");
-      command(reg::NVGAMCTRL, 14, "\xD0\x08\x10\x08\x06\x06\x39\x44\x51\x0b\x16\x14\x2f\x31");
-
-
-      // trigger "vsync" slightly earlier to avoid tearing while pixel-doubling
-      // (this is still outside of the visible part of the screen)
-      command(reg::STE, 2, "\x01\x2C");
-    }
-
-    if(width == 320 && height == 240) {
-      command(reg::PORCTRL, 5, "\x0c\x0c\x00\x33\x33");
-      command(reg::GCTRL, 1, "\x35");
-      command(reg::VCOMS, 1, "\x1f");
-      command(reg::LCMCTRL, 1, "\x2c");
-      command(reg::VDVVRHEN, 1, "\x01");
-      command(reg::VRHS, 1, "\x12");
-      command(reg::VDVS, 1, "\x20");
-      command(reg::PWCTRL1, 2, "\xa4\xa1");
-      command(0xd6, 1, "\xa1"); // ???
-      command(reg::PVGAMCTRL, 14, "\xD0\x08\x11\x08\x0C\x15\x39\x33\x50\x36\x13\x14\x29\x2D");
-      command(reg::NVGAMCTRL, 14, "\xD0\x08\x10\x08\x06\x06\x39\x44\x51\x0B\x16\x14\x2F\x31");
-    }
-
-    command(reg::FRCTRL2, 1, "\x15"); // 50Hz
-
-    command(reg::INVON);   // set inversion mode
-    command(reg::SLPOUT);  // leave sleep mode
-    command(reg::DISPON);  // turn display on
-
-    sleep_ms(100);
-
-    // setup correct addressing window
-    uint8_t madctl = MADCTL::RGB | rotations[LCD_ROTATION / 90];
-    if(width == 240 && height == 240) {
-      set_window(0, 0, 240, 240);
-    }
-
-    if(width == 240 && height == 135) {
-      set_window(40, 53, 240, 135);
-    }
-
-    if(width == 320 && height == 240) {
-      set_window(0, 0, 320, 240);
-    }
-
-    command(reg::MADCTL,    1, (char *)&madctl);
-  }
+  // send initialisation sequence for our standard displays based on the width and height
+  st7789_init_sequence();
 
   // initialise dma channel for transmitting pixel data to screen
   dma_channel = dma_claim_unused_channel(true);
@@ -303,174 +461,7 @@ static void init(bool auto_init_sequence = true) {
 
   irq_add_shared_handler(DMA_IRQ_0, st7789_dma_irq_handler, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
   irq_set_enabled(DMA_IRQ_0, true);
-}
 
-static void command(uint8_t command, size_t len, const char *data) {
-  pio_wait(pio, pio_sm);
-
-  if(write_mode) {
-    // reconfigure to 8 bits
-    pio_sm_set_enabled(pio, pio_sm, false);
-    pio->sm[pio_sm].shiftctrl &= ~PIO_SM0_SHIFTCTRL_PULL_THRESH_BITS;
-    pio->sm[pio_sm].shiftctrl |= (8 << PIO_SM0_SHIFTCTRL_PULL_THRESH_LSB) | PIO_SM0_SHIFTCTRL_AUTOPULL_BITS;
-
-    // switch back to raw
-    pio_sm_restart(pio, pio_sm);
-    pio_sm_set_wrap(pio, pio_sm, pio_offset + st7789_raw_wrap_target, pio_offset + st7789_raw_wrap);
-    pio_sm_exec(pio, pio_sm, pio_encode_jmp(pio_offset));
-
-    pio_sm_set_enabled(pio, pio_sm, true);
-    write_mode = false;
-  }
-
-  gpio_put(LCD_CS_PIN, 0);
-
-  gpio_put(LCD_DC_PIN, 0); // command mode
-  pio_put_byte(pio, pio_sm, command);
-
-  if(data) {
-    pio_wait(pio, pio_sm);
-    gpio_put(LCD_DC_PIN, 1); // data mode
-
-    for(size_t i = 0; i < len; i++)
-      pio_put_byte(pio, pio_sm, data[i]);
-  }
-
-  pio_wait(pio, pio_sm);
-  gpio_put(LCD_CS_PIN, 1);
-}
-
-static void update(bool dont_block = false) {
-  if(dma_channel_is_busy(dma_channel) && dont_block) {
-    return;
-  }
-
-  dma_channel_wait_for_finish_blocking(dma_channel);
-
-  if(!write_mode)
-    prepare_write();
-
-  if(pixel_double) {
-    cur_scanline = 0;
-    upd_frame_buffer = frame_buffer;
-    dma_channel_set_trans_count(dma_channel, win_w / 4, false);
-  } else
-    dma_channel_set_trans_count(dma_channel, win_w * win_h, false);
-
-  dma_channel_set_read_addr(dma_channel, frame_buffer, true);
-}
-
-static void set_backlight(uint8_t brightness) {
-#ifdef LCD_BACKLIGHT_PIN
-  // gamma correct the provided 0-255 brightness value onto a
-  // 0-65535 range for the pwm counter
-  float gamma = 2.8;
-  uint16_t value = (uint16_t)(pow((float)(brightness) / 255.0f, gamma) * 65535.0f + 0.5f);
-  pwm_set_gpio_level(LCD_BACKLIGHT_PIN, value);
-#endif
-}
-
-static bool vsync_callback(gpio_irq_callback_t callback) {
-#ifdef LCD_VSYNC_PIN
-  gpio_set_irq_enabled_with_callback(LCD_VSYNC_PIN, GPIO_IRQ_EDGE_RISE, true, callback);
-  return true;
-#else
-  return false;
-#endif
-}
-
-static void set_window(uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
-  uint32_t cols = __builtin_bswap32((x << 16) | (x + w - 1));
-  uint32_t rows = __builtin_bswap32((y << 16) | (y + h - 1));
-
-  command(reg::CASET, 4, (const char *)&cols);
-  command(reg::RASET, 4, (const char *)&rows);
-
-  win_w = w;
-  win_h = h;
-}
-
-static void set_pixel_double(bool pd) {
-  pixel_double = pd;
-
-  // nop to reconfigure PIO
-  if(write_mode)
-    command(0);
-
-  if(pixel_double) {
-    dma_channel_acknowledge_irq0(dma_channel);
-    dma_channel_set_irq0_enabled(dma_channel, true);
-  } else
-    dma_channel_set_irq0_enabled(dma_channel, false);
-}
-
-static void clear() {
-  if(!write_mode)
-    prepare_write();
-
-  for(int i = 0; i < win_w * win_h; i++)
-    pio_sm_put_blocking(pio, pio_sm, 0);
-}
-
-static bool dma_is_busy() {
-  if(pixel_double && cur_scanline <= win_h / 2)
-    return true;
-
-  return dma_channel_is_busy(dma_channel);
-}
-
-static void prepare_write() {
-  pio_wait(pio, pio_sm);
-
-  // setup for writing
-  uint8_t r = reg::RAMWR;
-  gpio_put(LCD_CS_PIN, 0);
-
-  gpio_put(LCD_DC_PIN, 0); // command mode
-  pio_put_byte(pio, pio_sm, r);
-  pio_wait(pio, pio_sm);
-
-  gpio_put(LCD_DC_PIN, 1); // data mode
-
-  pio_sm_set_enabled(pio, pio_sm, false);
-  pio_sm_restart(pio, pio_sm);
-
-  if(pixel_double) {
-    // switch program
-    pio_sm_set_wrap(pio, pio_sm, pio_double_offset + st7789_pixel_double_wrap_target, pio_double_offset + st7789_pixel_double_wrap);
-
-    // 32 bits, no autopull
-    pio->sm[pio_sm].shiftctrl &= ~(PIO_SM0_SHIFTCTRL_PULL_THRESH_BITS | PIO_SM0_SHIFTCTRL_AUTOPULL_BITS);
-
-    pio_sm_exec(pio, pio_sm, pio_encode_jmp(pio_double_offset));
-
-    // reconfigure dma size
-    dma_channel_hw_addr(dma_channel)->al1_ctrl &= ~DMA_CH0_CTRL_TRIG_DATA_SIZE_BITS;
-    dma_channel_hw_addr(dma_channel)->al1_ctrl |= DMA_SIZE_32 << DMA_CH0_CTRL_TRIG_DATA_SIZE_LSB;
-  } else {
-    // 16 bits, autopull
-    pio->sm[pio_sm].shiftctrl &= ~PIO_SM0_SHIFTCTRL_PULL_THRESH_BITS;
-    pio->sm[pio_sm].shiftctrl |= (16 << PIO_SM0_SHIFTCTRL_PULL_THRESH_LSB) | PIO_SM0_SHIFTCTRL_AUTOPULL_BITS;
-
-    dma_channel_hw_addr(dma_channel)->al1_ctrl &= ~DMA_CH0_CTRL_TRIG_DATA_SIZE_BITS;
-    dma_channel_hw_addr(dma_channel)->al1_ctrl |= DMA_SIZE_16 << DMA_CH0_CTRL_TRIG_DATA_SIZE_LSB;
-  }
-
-  pio_sm_set_enabled(pio, pio_sm, true);
-
-  write_mode = true;
-}
-
-static void vsync_callback(uint gpio, uint32_t events) {
-  if(!do_render && !dma_is_busy()) {
-    ::update();
-    do_render = true;
-  }
-}
-
-void init_display() {
-  frame_buffer = screen_fb;
-  ::init();
   clear();
 
   have_vsync = vsync_callback(vsync_callback);
