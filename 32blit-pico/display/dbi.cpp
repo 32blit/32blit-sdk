c375cdcc1143ff3be53a1e322385b09964857f1a
Charlie Birks <charlie@daft.games>
2024-08-31 19:18:23 +0100

    pico: rename the pixel-double irq handler

--- 32blit-pico/display/dbi.cpp
+++ 32blit-pico/display/dbi.cpp
@@ -84,7 +84,7 @@ static void pio_wait(PIO pio, uint sm) {
 }
 
 // used for pixel doubling
-static void __isr dbi_dma_irq_handler() {
+static void __isr double_dma_irq_handler() {
   if(dma_channel_get_irq0_status(dma_channel)) {
     dma_channel_acknowledge_irq0(dma_channel);
 
@@ -439,7 +439,7 @@ void init_display() {
   dma_channel_configure(
     dma_channel, &config, &pio->txf[pio_sm], frame_buffer, DISPLAY_WIDTH * DISPLAY_HEIGHT, false);
 
-  irq_add_shared_handler(DMA_IRQ_0, dbi_dma_irq_handler, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+  irq_add_shared_handler(DMA_IRQ_0, double_dma_irq_handler, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
   irq_set_enabled(DMA_IRQ_0, true);
 
   clear();
