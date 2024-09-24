17104cbfab226910f52b50fd12823a82c4a5a87d
Charlie Birks <charlie@daft.games>
2024-09-24 15:24:07 +0100

    Remove unimplemented world_to_scale declaration

--- 32blit/graphics/mode7.hpp
+++ 32blit/graphics/mode7.hpp
@@ -9,6 +9,5 @@ namespace blit {
 
   void mode7(Surface *dest, Surface *tiles, MapLayer *layer, float fov, float angle, Vec2 pos, float near, float far, Rect viewport);
   Vec2 world_to_screen(Vec2 w, float fov, float angle, Vec2 pos, float near, float far, Rect viewport);
-  float world_to_scale(Vec2 w, float fov, float angle, Vec2 pos, float near, float far, Rect viewport);
 
 }
