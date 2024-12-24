14d3fa03fe59f4e029bed261cc21142fbea06bb6
Charlie Birks <charlie@daft.games>
2024-12-24 15:56:21 +0000

    pico: restore i2c pullups
    PicoVision/seesaw/qwstpad all have external pull-ups. I forgot on all my boards...

--- 32blit-pico/main.cpp
+++ 32blit-pico/main.cpp
@@ -207,6 +207,9 @@ static void init_i2c() {
   i2c_init(i2c_default, DEFAULT_I2C_CLOCK);
   gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
   gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+
+  gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+  gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 #endif
 }
 
